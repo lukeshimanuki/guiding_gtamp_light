d2d3c36519e7dc09536639ba2487701f5055947e
beomjoon <beomjoon@mit.edu>
2019-10-15 20:57:50 -0400

    separate save dir for different dtypes

--- generators/learning/train_sampler.py
+++ generators/learning/train_sampler.py
@@ -130,8 +130,8 @@ def train_rel_konf_place_mse(config):
     n_key_configs = 615
     dim_state = (n_key_configs, 2, 1)
     dim_action = 4
-    savedir = 'generators/learning/learned_weights/state_data_mode_%s_action_data_mode_%s/rel_konf_place_mse/' % (
-        state_data_mode, action_data_mode)
+    savedir = 'generators/learning/learned_weights/dtype_%s_state_data_mode_%s_action_data_mode_%s/rel_konf_place_mse/' % (
+        config.dtype, state_data_mode, action_data_mode)
     admon = RelKonfMSEPose(dim_action=dim_action, dim_collision=dim_state,
                            save_folder=savedir, tau=config.tau, config=config)
     admon.policy_model.summary()
@@ -150,8 +150,8 @@ def train_rel_konf_place_admon(config):
     n_key_configs = 615
     dim_state = (n_key_configs, 2, 1)
     dim_action = 4
-    savedir = 'generators/learning/learned_weights/state_data_mode_%s_action_data_mode_%s/rel_konf_place_admon/' % (
-        state_data_mode, action_data_mode)
+    savedir = 'generators/learning/learned_weights/dtype_%s_state_data_mode_%s_action_data_mode_%s/rel_konf_place_admon/' % (
+        config.dtype, state_data_mode, action_data_mode)
     admon = RelKonfIMLEPose(dim_action=dim_action, dim_collision=dim_state,
                             save_folder=savedir, tau=config.tau, config=config)
     print "Created IMLE-admon"
