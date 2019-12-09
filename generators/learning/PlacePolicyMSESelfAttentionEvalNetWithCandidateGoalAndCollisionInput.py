c03a8cc7f8684c8af6fd3980b57fae81d5a6acab
beomjoon <beomjoon@mit.edu>
2019-12-09 09:01:12 -0500

    testing dense and conv nets equivalence

--- generators/learning/PlacePolicyMSESelfAttentionEvalNetWithCandidateGoalAndCollisionInput.py
+++ generators/learning/PlacePolicyMSESelfAttentionEvalNetWithCandidateGoalAndCollisionInput.py
@@ -1,6 +1,7 @@
 from keras.layers import *
 from keras.layers.merge import Concatenate
 from keras import backend as K
+from keras.models import Model
 
 from PlacePolicyMSESelfAttentionDenseEvalNet import PlacePolicyMSESelfAttentionDenseEvalNet
 
@@ -45,37 +46,54 @@ class PlacePolicyMSESelfAttentionEvalNetWithCandidateGoalAndCollisionInput(Place
     def construct_eval_net(self, candidate_qg_input):
         q0_qg_eval = self.construct_candidate_qg_from_q0_eval(candidate_qg_input)
         collision_input = Flatten()(self.collision_input)
+        dense_num = 8
+        #collision_input = Concatenate(axis=1, name='collision_input')([collision_input_1, self.pose_input])
+        # May be reduce the dimensionality here?
+        evalnet = Dense(dense_num, activation='relu',
+                        kernel_initializer=self.kernel_initializer,
+                        bias_initializer=self.bias_initializer)(collision_input)
+        evalnet = Dense(dense_num, activation='relu',
+                        kernel_initializer=self.kernel_initializer,
+                        bias_initializer=self.bias_initializer)(evalnet)
+        evalnet = Dense(615, activation='linear',
+                        kernel_initializer=self.kernel_initializer,
+                        bias_initializer=self.bias_initializer)(evalnet)
+
         collision_input = RepeatVector(615)(collision_input)
-        collision_input = Reshape((615, 615, 2))(collision_input)
-        n_filters = 32
+        collision_input = Reshape((615, 615*2, 1))(collision_input)
         n_dim = collision_input.shape[2]._value
-        H = Conv2D(filters=n_filters,
+        H = Conv2D(filters=dense_num,
                    kernel_size=(1, n_dim),
                    strides=(1, 1),
                    activation='relu',
                    kernel_initializer=self.kernel_initializer,
                    bias_initializer=self.bias_initializer)(collision_input)
-        for _ in range(2):
-            H = Conv2D(filters=32,
-                       kernel_size=(1, 1),
-                       strides=(1, 1),
-                       activation='relu',
-                       kernel_initializer=self.kernel_initializer,
-                       bias_initializer=self.bias_initializer)(H)
-        q0_qg_eval = Conv2D(filters=1,
-                            kernel_size=(1, 1),
-                            strides=(1, 1),
-                            activation='linear',
-                            kernel_initializer=self.kernel_initializer,
-                            bias_initializer=self.bias_initializer,
-                            name='q0_qg_eval')(H)
-        q0_qg_eval = Reshape((615,))(q0_qg_eval)
-        evalnet = q0_qg_eval
+        H = Conv2D(filters=dense_num,
+                   kernel_size=(1, 1),
+                   strides=(1, 1),
+                   activation='relu',
+                   kernel_initializer=self.kernel_initializer,
+                   bias_initializer=self.bias_initializer)(H)
+        H = Conv2D(filters=615,
+                   kernel_size=(1, 1),
+                   strides=(1, 1),
+                   activation='linear',
+                   kernel_initializer=self.kernel_initializer,
+                   bias_initializer=self.bias_initializer,
+                   name='q0_qg_eval')(H)
+
+        def take_the_first_row(x):
+            return x[:, 0]
+        H = Lambda(take_the_first_row, name='take_the_first_row')(H)
+        H = Reshape((615,))(H)
+        conv_evalnet = H
 
         def compute_softmax(x):
             return K.softmax(x, axis=-1)
+
+        evalnet = Lambda(compute_softmax, name='softmax')(conv_evalnet)
+        conv_model = Model(inputs=[self.goal_flag_input, self.key_config_input, self.collision_input, self.pose_input], outputs=[evalnet])
+        dense_model = Model(inputs=[self.goal_flag_input, self.key_config_input, self.collision_input, self.pose_input], outputs=[evalnet])
+
         import pdb;pdb.set_trace()
-
-        evalnet = Lambda(compute_softmax, name='softmax')(evalnet)
-
         return evalnet
