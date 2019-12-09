d88ac2806420bb6ef48ba441547b4b1bc63b4576
beomjoon <beomjoon@mit.edu>
2019-12-09 09:04:25 -0500

    adding pose information

--- generators/learning/PlacePolicyMSESelfAttentionEvalNetWithCandidateGoalAndCollisionInput.py
+++ generators/learning/PlacePolicyMSESelfAttentionEvalNetWithCandidateGoalAndCollisionInput.py
@@ -16,6 +16,8 @@ class PlacePolicyMSESelfAttentionEvalNetWithCandidateGoalAndCollisionInput(Place
         pose_input = RepeatVector(615)(self.pose_input)
         pose_input = Reshape((615, 4, 1))(pose_input)
         concat_input = Concatenate(axis=2)([candidate_qg_input, pose_input])
+        return concat_input
+        """
         n_dim = concat_input.shape[2]._value
         n_filters = 32
         H = Conv2D(filters=n_filters,
@@ -42,6 +44,7 @@ class PlacePolicyMSESelfAttentionEvalNetWithCandidateGoalAndCollisionInput(Place
         #q0_qg_eval = RepeatVector(2)(q0_qg_eval)
         #q0_qg_eval = Reshape((615, 2, 1))(q0_qg_eval)
         return q0_qg_eval
+        """
 
     def construct_eval_net(self, candidate_qg_input):
         q0_qg_eval = self.construct_candidate_qg_from_q0_eval(candidate_qg_input)
@@ -49,6 +52,7 @@ class PlacePolicyMSESelfAttentionEvalNetWithCandidateGoalAndCollisionInput(Place
         dense_num = 8
         #collision_input = Concatenate(axis=1, name='collision_input')([collision_input_1, self.pose_input])
         # May be reduce the dimensionality here?
+        """
         evalnet = Dense(dense_num, activation='relu',
                         kernel_initializer=self.kernel_initializer,
                         bias_initializer=self.bias_initializer)(collision_input)
@@ -58,16 +62,20 @@ class PlacePolicyMSESelfAttentionEvalNetWithCandidateGoalAndCollisionInput(Place
         evalnet = Dense(615, activation='linear',
                         kernel_initializer=self.kernel_initializer,
                         bias_initializer=self.bias_initializer)(evalnet)
+        """
 
         collision_input = RepeatVector(615)(collision_input)
         collision_input = Reshape((615, 615*2, 1))(collision_input)
-        n_dim = collision_input.shape[2]._value
+        concat_input = Concatenate(axis=2)([collision_input, q0_qg_eval])
+
+        # todo: now to this, I will attach the q0 qg information
+        n_dim = concat_input.shape[2]._value
         H = Conv2D(filters=dense_num,
                    kernel_size=(1, n_dim),
                    strides=(1, 1),
                    activation='relu',
                    kernel_initializer=self.kernel_initializer,
-                   bias_initializer=self.bias_initializer)(collision_input)
+                   bias_initializer=self.bias_initializer)(concat_input)
         H = Conv2D(filters=dense_num,
                    kernel_size=(1, 1),
                    strides=(1, 1),
