af427825d8ba1d249e022ba1369eb77b263b6c3a
Beomjoon Kim <beomjoon@phaedra.csail.mit.edu>
2019-12-22 13:59:28 -0500

    changing the loss weights

--- generators/learning/PlacePolicyMSECombinationOfQg.py
+++ generators/learning/PlacePolicyMSECombinationOfQg.py
@@ -59,7 +59,7 @@ class PlacePolicyMSECombinationOfQg(PlacePolicyMSE):
         def custom_mse(y_true, y_pred):
             return tf.reduce_mean(tf.norm(y_true - y_pred, axis=-1))
 
-        model.compile(loss=[lambda _, pred: pred, 'mse'], optimizer=self.opt_D, loss_weights=[10, 1])
+        model.compile(loss=[lambda _, pred: pred, 'mse'], optimizer=self.opt_D, loss_weights=[10, 1, 1])
         #model.compile(loss='mse', optimizer=self.opt_D)
         return model
 
