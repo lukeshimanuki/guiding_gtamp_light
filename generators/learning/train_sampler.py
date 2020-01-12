79511f3a21034e3c573995866236e793a691d482
Beomjoon Kim <beomjoon@phaedra.csail.mit.edu>
2020-01-12 12:28:55 -0500

    either filtered or unfiltered

--- generators/learning/train_sampler.py
+++ generators/learning/train_sampler.py
@@ -57,7 +57,7 @@ def load_data(traj_dir, action_type, desired_region):
                                                                                          action_data_mode,
                                                                                          action_type,
                                                                                          desired_region)
-        cache_file_name = 'cache_smode_%s_amode_%s_atype_%s_region_%s.pkl' % (state_data_mode,
+        cache_file_name = 'cache_smode_%s_amode_%s_atype_%s_region_%s_unfiltered.pkl' % (state_data_mode,
                                                                                          action_data_mode,
                                                                                          action_type,
                                                                                          desired_region)
