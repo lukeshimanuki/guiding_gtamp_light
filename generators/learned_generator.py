80f8772d3ed660dd2e205d85b684c212211e6251
beomjoon <beomjoon@mit.edu>
2020-01-22 09:02:51 -0500

    on-demand learned generator

--- generators/learned_generator.py
+++ generators/learned_generator.py
@@ -4,18 +4,18 @@ from trajectory_representation.concrete_node_state import ConcreteNodeState
 from generators.learning.utils import data_processing_utils
 
 from gtamp_utils import utils
-from generators.learning.utils.sampler_utils import generate_pick_and_place_batch
-from generators.learning.utils.sampler_utils import unprocess_pick_and_place_smpls
+
+from generators.learning.utils.sampler_utils import generate_pick_and_place_batch, prepare_input_except_noise
+from generators.learning.utils.sampler_utils import unprocess_pick_and_place_smpls, get_indices_to_delete
 from generators.learning.PlacePolicyIMLE import uniform_noise
 from generators.learning.PlacePolicyIMLE import gaussian_noise
 
 import time
 import numpy as np
+import copy
 import pickle
 
-
-
-#noise = uniform_noise
+# noise = uniform_noise
 noise = gaussian_noise
 
 
@@ -41,16 +41,26 @@ class LearnedGenerator(PaPUniformGenerator):
         self.noises_used = []
         self.tried_smpls = []
 
+        self.pick_input = prepare_input_except_noise(self.smpler_state, delete=True, region='loading_region',
+                                                     filter_konfs=False)
+        self.place_input = prepare_input_except_noise(self.smpler_state, delete=True,
+                                                      region=operator_skeleton.discrete_parameters['place_region'],
+                                                      filter_konfs=False)
+
         # to do generate 1000 smpls here
-        n_total_iters = sum(range(10, self.max_n_iter, 10))
-
-        z_smpls = noise(z_size=(1900, 7))
-        stime=time.time()
-        smpls = generate_pick_and_place_batch(self.smpler_state, self.sampler, z_smpls)
-        self.policy_smpl_batch = unprocess_pick_and_place_smpls(smpls)
-        print "Prediction time", time.time() - stime
-
-        self.policy_smpl_idx = 0
+        # n_total_iters = sum(range(10, self.max_n_iter, 10))
+        # z_smpls = noise(z_size=(1900, 7))
+        # stime = time.time()
+        # smpls = generate_pick_and_place_batch(self.smpler_state, self.sampler, z_smpls)
+        # self.policy_smpl_batch = unprocess_pick_and_place_smpls(smpls)
+        # print "Prediction time", time.time() - stime
+        """
+        orig_color = utils.get_color_of(self.obj)
+        utils.set_color(self.obj, [0, 1, 0])
+        utils.visualize_placements(self.policy_smpl_batch[0:100, -3:], self.obj)
+        utils.set_color(self.obj, orig_color)
+        """
+        # self.policy_smpl_idx = 0
 
     def process_abstract_state_collisions_into_key_config_obstacles(self, abstract_state):
         # todo prevent collision with the target object
@@ -66,7 +76,6 @@ class LearnedGenerator(PaPUniformGenerator):
         self.problem_env.env.GetKinBody(self.obj).Enable(False)
         collision_vector2 = utils.compute_occ_vec(key_configs)
         self.problem_env.env.GetKinBody(self.obj).Enable(True)
-
         assert np.all(collision_vector == collision_vector2)
         """
 
@@ -91,24 +100,6 @@ class LearnedGenerator(PaPUniformGenerator):
         # Right now, I am discarding the entire sample
         # But really, I should learn to predict both the pick and place,
         # so that if the place paired with the pick is infeasible, then I can discard both samples
-        """
-        if action_data_mode == 'pick_parameters_place_relative_to_object':
-            place_smpl = self.policy_smpl_batch[self.policy_smpl_idx]
-            place_smpl = data_processing_utils.get_absolute_placement_from_relative_placement(place_smpl,
-                                                                                              self.smpler_state.abs_obj_pose)
-            self.policy_smpl_idx += 1
-        elif action_data_mode == 'pick_abs_base_pose_place_abs_obj_pose':
-            place_smpl = self.policy_smpl_batch[self.policy_smpl_idx]
-            self.policy_smpl_idx += 1
-            if self.policy_smpl_idx >= len(self.policy_smpl_batch):
-                z_smpls = uniform_noise(z_size=(500, 4))
-                stime = time.time()
-                self.policy_smpl_batch = generate_smpl_batch(self.smpler_state, self.sampler, z_smpls, self.key_configs)
-                print "Prediction time for further sampling", time.time() - stime
-                self.policy_smpl_idx = 0
-        else:
-            raise NotImplementedError
-        """
         smpl = self.policy_smpl_batch[self.policy_smpl_idx]
         self.policy_smpl_idx += 1
         if self.policy_smpl_idx >= len(self.policy_smpl_batch):
@@ -119,9 +110,37 @@ class LearnedGenerator(PaPUniformGenerator):
             print "Prediction time for further sampling", time.time() - stime
             self.policy_smpl_idx = 0
         self.tried_smpls.append(smpl)
-        #parameters = self.sample_from_uniform()
-        #parameters[6:] = place_smpl
+        # parameters = self.sample_from_uniform()
+        # parameters[6:] = place_smpl
         return smpl
+
+    def sample_from_pick_sampler(self):
+        pick_noise = noise(z_size=(1, 7))
+        # todo fix the pick_input delete option
+        inp = [self.pick_input['goal_flags'], self.pick_input['key_configs'],
+               self.pick_input['collisions'], self.pick_input['poses'],
+               pick_noise]
+        pick_pred = self.sampler['pick'].policy_model.predict(inp).squeeze()
+        ir_parameters = pick_pred[3:]
+        portion = ir_parameters[0]
+        base_angle = utils.decode_sin_and_cos_to_angle(ir_parameters[1:3])
+        facing_angle_offset = ir_parameters[3]
+        pick_param = np.hstack([pick_pred[:3], portion, base_angle, facing_angle_offset])
+        return pick_param
+
+    def sample_from_place_sampler(self, operator_skeleton, pick_base_pose):
+        place_noise = noise(z_size=(1, 4))
+
+        # setup the poses from pick
+        poses = self.place_input['poses']
+        pick_base_pose = utils.encode_pose_with_sin_and_cos_angle(pick_base_pose)
+        poses[:, -4:] = pick_base_pose
+        self.place_input['poses'] = poses
+
+        inp = [self.place_input['goal_flags'], self.place_input['key_configs'], self.place_input['collisions'], self.place_input['poses'], place_noise]
+        place_pred = self.sampler['place'].policy_model.predict(inp)
+        place_pred = utils.decode_pose_with_sin_and_cos_angle(place_pred)
+        return place_pred
 
     def sample_feasible_op_parameters(self, operator_skeleton, n_iter, n_parameters_to_try_motion_planning):
         assert n_iter > 0
@@ -129,19 +148,41 @@ class LearnedGenerator(PaPUniformGenerator):
         obj = operator_skeleton.discrete_parameters['object']
 
         orig_color = utils.get_color_of(obj)
+        stime = time.time()
         for i in range(n_iter):
-            stime = time.time()
+            """
             op_parameters = self.sample_from_learned_samplers()
             op_parameters, status = self.op_feasibility_checker.check_feasibility(operator_skeleton,
                                                                                   op_parameters,
                                                                                   self.swept_volume_constraint,
                                                                                   parameter_mode='obj_pose')
-            smpling_time = time.time() - stime
-            self.smpling_time.append(smpling_time)
+            """
+
+            pick_op_parameters = self.sample_from_pick_sampler()
+            pick_parameters, pick_status = self.op_feasibility_checker.check_pick_feasible(pick_op_parameters,
+                                                                                           operator_skeleton)
+            if pick_status != 'HasSolution':
+                continue
+
+            place_op_parameters = self.sample_from_place_sampler(operator_skeleton, pick_parameters['q_goal'])
+            place_parameters, place_status = self.op_feasibility_checker.check_place_feasible(pick_parameters,
+                                                                                              place_op_parameters,
+                                                                                              operator_skeleton,
+                                                                                              parameter_mode='obj_pose')
+            if place_status != 'HasSolution':
+                continue
+
+            status = "HasSolution"
+            op_parameters = {'pick': pick_parameters, 'place': place_parameters}
             if status == 'HasSolution':
                 feasible_op_parameters.append(op_parameters)
                 if len(feasible_op_parameters) >= n_parameters_to_try_motion_planning:
-                    break
+                    #break
+                    pass
+
+        smpling_time = time.time() - stime
+        print "Sampling time", smpling_time
+        import pdb;pdb.set_trace()
 
         if len(feasible_op_parameters) == 0:
             feasible_op_parameters.append(op_parameters)  # place holder
