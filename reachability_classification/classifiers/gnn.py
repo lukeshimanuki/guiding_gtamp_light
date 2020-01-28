8611353bf6d86eb0b9feb91bdbbe8cd2bb363363
beomjoon <beomjoon@mit.edu>
2020-01-28 10:35:31 -0500

    removing comments

--- reachability_classification/classifiers/gnn.py
+++ reachability_classification/classifiers/gnn.py
@@ -39,8 +39,6 @@ class GNNReachabilityNet(nn.Module):
             torch.nn.Conv2d(1, out_channels_1, kernel_size=(in_channels, 1), stride=1),
             nn.ReLU(),
             torch.nn.Conv2d(out_channels_1, out_channels, kernel_size=(1, 1), stride=1),
-            # if all weights are negative, then this will create problems.
-            # how can I resolve this probelm? Use leaky relu
             nn.LeakyReLU()
         )
 
@@ -72,45 +70,3 @@ class GNNReachabilityNet(nn.Module):
         final_vertex_output = self.vertex_output_lin(agg).squeeze()
         graph_output = self.graph_output_lin(final_vertex_output)
         return graph_output
-
-"""
-x = torch.rand(618, 11)  # but then how do I create multiple data points?
-edge_idx = torch.tensor([[0, 1, 2],  # it's always bidrectional
-                         [1, 2, 1]],
-                        dtype=torch.long)  # 2 x n_edges, top indicating the src and bottom indicating the dest
-
-data = {'vtxs': x}
-data_list = [data] * 10000
-loader = torch.utils.data.DataLoader(data_list, batch_size=32, shuffle=True, num_workers=2)
-net = GNNReachabilityNet()
-optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-device = torch.device('cpu')
-
-
-def train(epoch):
-    net.to(device)
-    net.train()
-    total_loss = 0
-    for data in loader:
-        data['vtxs'].to(device)
-        optimizer.zero_grad()
-        out = net(data['vtxs'])
-        loss = ((out - data.y).pow(2)).mean()
-        loss.backward()
-        optimizer.step()
-
-    import pdb;
-    pdb.set_trace()
-    return total_loss / len(train_loader.dataset)
-
-
-train(1)
-
-# I need to put it through a dense net to produce results
-
-# How do I do:
-#   1. Aggregating all the outputs for final prediction? Take the output from net, and put it through a dense net
-#   2. Multiple rounds of msg passing? I guess I take the output and put it through the net again? This requires
-#       maintaining the same output value. What I perhaps want to do is to create another network that now takes the
-#       graph data.
-"""
