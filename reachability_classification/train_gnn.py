2ba2d99915636ebba04ced1acb390e4daf2d978a
Beomjoon Kim <beomjoon@phaedra.csail.mit.edu>
2020-01-28 10:12:09 -0500

    fixing the train_gnn to use multiple workers

--- reachability_classification/train_gnn.py
+++ reachability_classification/train_gnn.py
@@ -19,8 +19,8 @@ def main():
     print device
 
     dataset = GNNReachabilityDataset()
-    n_train = int(len(dataset) * 0.4)
-    trainset, testset = torch.utils.data.random_split(dataset, [3433, len(dataset)-3433])
+    n_train = int(len(dataset) * 0.9)
+    trainset, testset = torch.utils.data.random_split(dataset, [n_train, len(dataset)-n_train])
     print "N_train", len(trainset)
 
     net = GNNReachabilityNet(trainset[1]['edges'], n_key_configs=618, device=device)
@@ -29,9 +29,9 @@ def main():
 
     learning_rate = 1e-3
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, num_workers=20, pin_memory=True)
     acc_list = []
-
+    
     test_vertices = torch.from_numpy(testset.dataset[testset.indices[0:100]]['vertex']).float().to(device)
     test_labels = testset.dataset[testset.indices[0:100]]['y'].to(device)
     test_pred = net(test_vertices)
