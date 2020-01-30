fd20c970d4fbea75ffac2889670ca15a856f7d09
beomjoon <beomjoon@mit.edu>
2020-01-30 10:31:33 -0500

    download scripts

---
+++ download_mp_data.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+scp -r beomjoon@phaedra.csail.mit.edu:/home/beomjoon/guiding_gtamp_light/planning_experience/processed/motion_plans ./planning_experience/processed/
---
+++ download_reachability_weights.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+scp -r beomjoon@phaedra.csail.mit.edu:/home/beomjoon/guiding_gtamp_light/reachability_classification/weights/* ./reachability_classification/weights/
