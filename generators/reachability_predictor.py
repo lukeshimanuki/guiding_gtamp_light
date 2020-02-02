3e138b1fc9919f44edf0afef81b55faaf08d454d
beomjoon <beomjoon@mit.edu>
2020-02-02 08:06:54 -0500

    reachability predictor update

--- generators/reachability_predictor.py
+++ generators/reachability_predictor.py
@@ -4,8 +4,9 @@ import torch
 
 
 class ReachabilityPredictor:
-    def __init__(self, net):
-        self.net = net
+    def __init__(self, pick_net, place_net):
+        self.pick_net = pick_net
+        self.place_net = place_net
 
     def make_vertices(self, qg, key_configs, collisions):
         q0 = utils.get_robot_xytheta().squeeze()
@@ -16,12 +17,31 @@ class ReachabilityPredictor:
         v = torch.from_numpy(v).float()
         return v
 
-    def predict(self, qg, abstract_state):
+    def predict(self, op_parameters, abstract_state, abstract_action):
         collisions = self.process_abstract_state_collisions_into_key_config_obstacles(abstract_state)
         key_configs = abstract_state.prm_vertices
-        v = self.make_vertices(qg, key_configs, collisions)
 
-        return self.net(v)
+        pick_qg = op_parameters['pick']['q_goal']
+        v = self.make_vertices(pick_qg, key_configs, collisions)
+
+        is_pick_reachable = ((self.pick_net(v) > 0.5).cpu().numpy() == True)[0, 0]
+
+        if is_pick_reachable:
+            # I have to hold the object and check collisions
+            orig_config = utils.get_robot_xytheta()
+            target_obj = abstract_action.discrete_parameters['object']
+            utils.two_arm_pick_object(target_obj, {'q_goal': pick_qg})
+            collisions = utils.compute_occ_vec(key_configs)
+            collisions = utils.convert_binary_vec_to_one_hot(collisions)
+            utils.two_arm_place_object({'q_goal': pick_qg})
+            utils.set_robot_config(orig_config)
+            place_qg = op_parameters['place']['q_goal']
+            v = self.make_vertices(place_qg, key_configs, collisions)
+            pred = self.place_net(v)
+            is_place_reachable = ((pred > 0.5).cpu().numpy() == True)[0, 0]
+            return is_place_reachable
+        else:
+            return False
 
     def get_colliding_prm_idxs(self, abstract_state):
         colliding_vtx_idxs = []
