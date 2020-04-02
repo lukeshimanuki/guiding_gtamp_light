79bf3e5a20667dbd058124e0ecbacc99f59cb6b9
beomjoon <beomjoon@mit.edu>
2020-04-02 19:46:32 -0400

    factoring data loading in train torch sampler

--- generators/learning/train_torch_sampler.py
+++ generators/learning/train_torch_sampler.py
@@ -4,7 +4,6 @@ import torch
 from datasets.GeneratorDataset import StandardDataset
 from generators.learning.PlaceWGANGP import PlaceWGANgp
 
-
 if socket.gethostname() == 'lab' or socket.gethostname() == 'phaedra' or socket.gethostname() == 'dell-XPS-15-9560':
     ROOTDIR = './'
 else:
@@ -13,8 +12,19 @@ else:
 
 def save_weights(net, epoch, action_type, seed, region):
     net_name = net.__class__._get_name(net)
-    PATH = './learning/torch_weights/atype_%s_%s_region_%s_seed_%d_epoch_%d.pt' % (action_type, net_name, region, seed, epoch)
+    PATH = './learning/torch_weights/atype_%s_%s_region_%s_seed_%d_epoch_%d.pt' % (
+    action_type, net_name, region, seed, epoch)
     torch.save(net.state_dict(), PATH)
+
+
+def get_data_generator(action_type, region):
+    dataset = StandardDataset(action_type, region, True)
+    n_train = int(len(dataset) * 0.9)
+    trainset, testset = torch.utils.data.random_split(dataset, [n_train, len(dataset) - n_train])
+    batch_size = 32
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=20,
+                                              pin_memory=True)
+    return trainloader, trainset, testset
 
 
 def main():
@@ -22,22 +32,17 @@ def main():
         device = torch.device("cpu")
     else:
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = PlaceWGANgp('torch')
 
     print device
     seed = 0
     torch.cuda.manual_seed_all(seed)
+    torch.manual_seed(seed)
 
     action_type = 'place'
     region = 'loading_region'
-    dataset = StandardDataset(action_type, region, True)
-
-    n_train = int(len(dataset) * 100)
-    trainset, testset = torch.utils.data.random_split(dataset, [n_train, len(dataset) - n_train])
-
-    batch_size = 32
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=20,
-                                              pin_memory=True)
+    trainloader, trainset, testset = get_data_generator(action_type, region)
+    model = PlaceWGANgp(region)
+    n_train = len(trainset)
     model.train(trainloader, testset, n_train)
 
 
