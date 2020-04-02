5aa6e93d025102e32cbdc25075de62b2666fdf30
beomjoon <beomjoon@mit.edu>
2020-04-02 19:45:48 -0400

    adding evaluation script for wgangp

--- generators/learning/PlaceWGANGP.py
+++ generators/learning/PlaceWGANGP.py
@@ -2,7 +2,9 @@ import torch
 import torch.autograd as autograd
 import torch.optim as optim
 import numpy as np
+from sklearn.neighbors import KernelDensity
 import os
+import scipy as sp
 
 from torch import nn
 
@@ -47,7 +49,7 @@ class Discriminator(nn.Module):
             )
 
     def forward(self, action, konf, pose):
-        konf = konf.view((-1, 618*2))
+        konf = konf.view((-1, 618 * 2))
         konf_val = self.konf_net(konf)
         pose_val = self.pose_net(pose)
         action_val = self.action_net(action)
@@ -78,7 +80,7 @@ class Generator(nn.Module):
             )
 
         dim_actions = dim_data
-        dim_input = n_hidden * 2
+        dim_input = n_hidden * 2 + dim_actions
         self.output = \
             nn.Sequential(
                 torch.nn.Linear(dim_input, n_hidden),
@@ -87,10 +89,11 @@ class Generator(nn.Module):
             )
 
     def forward(self, konf, pose, noise):
-        konf = konf.view((-1, 618*2))
+        konf = konf.view((-1, 618 * 2))
         konf_val = self.konf_net(konf)
         pose_val = self.pose_net(pose)
-        concat = torch.cat((konf_val, pose_val), -1)
+        concat = torch.cat((konf_val, pose_val, noise), -1)
+
         return self.output(concat)
 
 
@@ -116,12 +119,19 @@ def calc_gradient_penalty(discriminator, actions_v, konf_obsts_v, poses_v, fake_
 
 
 class PlaceWGANgp:
-    def __init__(self, problem_name):
+    def __init__(self, region_name):
         self.n_dim_actions = 4
         self.discriminator = Discriminator(self.n_dim_actions)
         self.generator = Generator(self.n_dim_actions)
-        self.problem_name = problem_name
-        self.weight_dir = './weights/wgangp/' + problem_name + '/'
+        self.region_name = region_name
+        self.weight_dir = './weights/wgangp/' + 'torch' + '/'
+
+        if region_name == 'loading_region':
+            self.domain = np.array(
+                [[-0.34469225, -8.14641946, -1., -0.99999925], [3.92354742, -5.25567767, 1., 0.99999993]])
+        else:
+            self.domain = np.array(
+                [[-1.28392928, -2.95494754, -0.99999998, -0.99999999], [5.01948716, 2.58819546, 1., 1.]])
 
         if not os.path.isdir(self.weight_dir):
             os.makedirs(self.weight_dir)
@@ -139,6 +149,60 @@ class PlaceWGANgp:
         self.generator.load_state_dict(torch.load(weight_file))
         weight_file = self.weight_dir + '/disc_iter_%d.pt' % iteration
         self.discriminator.load_state_dict(torch.load(weight_file))
+
+    def evaluate_generator(self, test_data, iteration=None):
+        is_load_weights = iteration is not None
+        if is_load_weights:
+            self.load_weights(iteration)
+        test_data = test_data[:]
+        poses = torch.from_numpy(test_data['poses']).float()
+        konf_obsts = torch.from_numpy(test_data['konf_obsts']).float()
+
+        n_data = len(poses)
+        n_smpls_per_state = 100
+        smpls = []
+        for _ in range(n_smpls_per_state):
+            noise = torch.randn(n_data, 4)
+            new_smpls = self.generator(konf_obsts, poses, noise)
+            smpls.append(new_smpls)
+        smpls = torch.stack(smpls)
+
+        real_actions = test_data['actions']
+        real_mean = real_actions.mean(axis=0)
+        real_std = real_actions.std(axis=0)
+        real_actions = (real_actions - real_mean) / real_std
+        real_data_scores = []
+        entropies = []
+        min_mses = []
+        for idx in range(n_data):
+            smpls_from_state = smpls[:, idx, :]
+            smpls_from_state = smpls_from_state.cpu().detach().numpy()
+            smpls_from_state = (smpls_from_state - real_mean) / real_std
+            real_action = real_actions[idx].reshape(-1, self.n_dim_actions)
+
+            # measure the MSE
+            dists = ((real_action * real_std + real_mean) - (smpls_from_state * real_std + real_mean)) ** 2
+            dists = np.sqrt(np.sum(dists[:, 0:2], axis=-1))
+            min_mse = min(dists)
+            min_mses.append(min_mse)
+
+            # fit the KDE
+            generated_model = KernelDensity(kernel='gaussian', bandwidth=0.1).fit(smpls_from_state)
+            real_data_scores.append(generated_model.score(real_action))
+
+            # measure the entropy
+            smpls_from_state = smpls_from_state * real_std + real_mean
+            H, _, _ = np.histogram2d(smpls_from_state[:, 0], smpls_from_state[:, 1], bins=10,
+                                     range=self.domain[:, 0:2].transpose())
+            all_smpls_out_of_range = np.sum(H) == 0
+            if all_smpls_out_of_range:
+                entropy = np.inf
+            else:
+                prob = H / np.sum(H)
+                entropy = sp.stats.entropy(prob.flatten())
+            entropies.append(entropy)
+        print "MSE {:.2f} KDE_score {:.2f} Entropy {}".format(np.mean(min_mses), np.mean(real_data_scores), np.mean(entropies))
+        return np.mean(min_mses), np.mean(real_data_scores), np.mean(entropies)
 
     def train(self, data_loader, test_set, n_train):
         batch_size = 32  # Batch size
@@ -182,7 +246,6 @@ class PlaceWGANgp:
                 poses = _data['poses'].float()
                 konf_obsts = _data['konf_obsts'].float()
                 actions = _data['actions'].float()
-                #real_data = torch.Tensor(_data)
                 if use_cuda:
                     poses = poses.cuda()
                     konf_obsts = konf_obsts.cuda()
@@ -199,7 +262,7 @@ class PlaceWGANgp:
                 D_real.backward(mone)
 
                 # train with fake
-                noise = torch.randn(len(_data), n_data_dim)
+                noise = torch.randn(len(poses), n_data_dim)
                 if use_cuda:
                     noise = noise.cuda()
                 noisev = autograd.Variable(noise, volatile=True)  # totally freeze self.generator
@@ -210,7 +273,8 @@ class PlaceWGANgp:
                 D_fake.backward(one)
 
                 # train with gradient penalty
-                gradient_penalty = calc_gradient_penalty(self.discriminator, actions_v.data, konf_obsts_v, poses_v, fake.data, batch_size)
+                gradient_penalty = calc_gradient_penalty(self.discriminator, actions_v.data, konf_obsts_v, poses_v,
+                                                         fake.data, batch_size)
                 gradient_penalty.backward()
 
                 D_cost = D_fake - D_real + gradient_penalty
@@ -224,20 +288,18 @@ class PlaceWGANgp:
             poses = _data['poses'].float()
             konf_obsts = _data['konf_obsts'].float()
             actions = _data['actions'].float()
-            # real_data = torch.Tensor(_data)
             if use_cuda:
                 poses = poses.cuda()
                 konf_obsts = konf_obsts.cuda()
                 actions = actions.cuda()
             poses_v = autograd.Variable(poses)
             konf_obsts_v = autograd.Variable(konf_obsts)
-            actions_v = autograd.Variable(actions)
 
             for p in self.discriminator.parameters():
                 p.requires_grad = False  # to avoid computation
             self.generator.zero_grad()
 
-            noise = torch.randn(batch_size, n_data_dim)
+            noise = torch.randn(len(poses), n_data_dim)
             if use_cuda:
                 noise = noise.cuda()
             noisev = autograd.Variable(noise)
@@ -256,15 +318,14 @@ class PlaceWGANgp:
                 path = self.weight_dir + '/gen_iter_%d.pt' % iteration
                 torch.save(self.generator.state_dict(), path)
 
-                test_data = test_set.dataset[:]
-                poses = torch.from_numpy(test_data['poses']).float()
-                konf_obsts = torch.from_numpy(test_data['konf_obsts']).float()
-                actions = torch.from_numpy(test_data['actions']).float()
-                noise = torch.randn(batch_size, n_data_dim)
-                fake_actions = self.generator(konf_obsts, poses, noise)
-                mse_loss = torch.nn.MSELoss()
-                print mse_loss(fake_actions, actions).detach()
+                # self.evaluate_generator(test_set)
 
                 # How can I evaluate what's going on?
                 # Like, is mean MSE of 1.4818 good or bad?
                 # Also, how big is its entropy?
+                # One potential idea is to:
+                #   - Take test states
+                #   - Generate 100 samples each
+                #   - Fit the data using KDE
+                #   - Accuracy: measure the actual action's probability
+                #   - Diversity: measure the spread by binning the actions and looking at entropy
