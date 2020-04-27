82083a339120a48622723c9bd1f0a23ee7e3ea10
beomjoon <beomjoon@mit.edu>
2020-04-27 15:52:34 -0400

    adding temporarily feasible samples as best sample

--- generators/voo.py
+++ generators/voo.py
@@ -3,6 +3,7 @@ from feasibility_checkers.two_arm_pap_feasiblity_checker import TwoArmPaPFeasibi
 import numpy as np
 import time
 from gtamp_utils import utils
+
 IK_FEASIBLE_AND_BASEPOSE_COLLISION_FREE = 0
 INFEASIBLE = -1
 
@@ -30,10 +31,23 @@ class TwoArmVOOGenerator(Generator):
         orig_ik_checks = self.n_ik_checks
         self.feasibility_checker.feasible_pick = []
         print 'n tried samples', len(q_values)
+
+        evaled_values = q_values + self.basic_tested_sample_values
+        if len(evaled_values) > 0 and np.max(evaled_values) > self.sampler.infeasible_action_value:
+            evaled_x = actions + self.basic_tested_samples
+            best_evaled_x = evaled_x[np.argmax(evaled_values)]
+            distances = [self.sampler.distance_fn(best_evaled_x, x) for x in evaled_x]
+            n_smpls = 10 if len(distances) > 10 else len(distances)
+            k_nearest_neighbor_to_best_point = evaled_x[np.argsort(distances)][1:n_smpls]
+            self.sampler.k_nearest_neighbor_to_best_point = k_nearest_neighbor_to_best_point
+            import pdb;
+            pdb.set_trace()
+
         for _ in range(self.n_iter_limit):
             self.n_ik_checks += 1
-            sampled_op_parameters = self.sampler.sample(actions+self.basic_tested_samples,
-                                                        q_values+self.basic_tested_sample_values)
+            evaled_actions = actions + self.basic_tested_samples
+            evaled_values = q_values + self.basic_tested_sample_values
+            sampled_op_parameters = self.sampler.sample(evaled_actions, evaled_values)
 
             stime2 = time.time()
             op_parameters, status = self.feasibility_checker.check_feasibility(self.abstract_action,
@@ -43,7 +57,15 @@ class TwoArmVOOGenerator(Generator):
             if status == 'HasSolution':
                 # I should keep these. But their value needs to be updated if we ever get to try them.
                 # Let's not do it for now.
-                # But if I do this, I believe I can improve the first-encountered states too
+                # But if I do this, I believe I can improve the first-encountered states too.
+                # What value should this take?
+                #   If there is no feasible action, it should have the highest value
+                #   If there are feasible actions, then don't do this. You can follow them.
+                if np.max(evaled_values) == self.sampler.infeasible_action_value:
+                    sampled_op_parameters[0:6] = op_parameters['pick']['action_parameters']
+                    self.basic_tested_samples.append(sampled_op_parameters)
+                    self.basic_tested_sample_values.append(0)
+
                 feasible_op_parameters.append(op_parameters)
                 self.feasibility_checker.feasible_pick = []
                 if len(feasible_op_parameters) >= self.n_parameters_to_try_motion_planning:
@@ -59,7 +81,15 @@ class TwoArmVOOGenerator(Generator):
                     self.basic_tested_sample_values.append(self.sampler.infeasible_action_value)
 
         smpling_time = time.time() - stime
-        print "IK time {:.5f} Total IK checks {}".format(smpling_time, self.n_ik_checks-orig_ik_checks)
+        print "IK time {:.5f} Total IK checks {}".format(smpling_time, self.n_ik_checks - orig_ik_checks)
+
+        # Remove all the temporarily added ones
+        idxs_to_remove = np.where(self.basic_tested_sample_values == np.max(self.basic_tested_sample_values))[0]
+        for i in idxs_to_remove:
+            self.basic_tested_samples.pop(i)
+            self.basic_tested_sample_values.pop(i)
+        import pdb; pdb.set_trace()
+
         if len(feasible_op_parameters) == 0:
             feasible_op_parameters.append(op_parameters)  # place holder
             status = "NoSolution"
