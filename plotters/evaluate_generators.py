26d3f53cfb32f5fe2d2c1551978f35e2618ee875
beomjoon <beomjoon@mit.edu>
2020-05-08 08:35:41 -0400

    evaluate generator multithreaded

--- plotters/evaluate_generators.py
+++ plotters/evaluate_generators.py
@@ -29,33 +29,6 @@ def main():
         trainloader, trainset, testset = get_data_generator(config.atype, config.region)
         result = model.evaluate_generator(testset, iteration=config.iteration)
         pickle.dump(result, open(fdir + fname, 'wb'))
-    import pdb;
-    pdb.set_trace()
-
-    """
-    results = np.array(results)
-    iterations = np.array(iterations)
-    import pdb;pdb.set_trace()
-
-    # I need to select non-inf entropy
-    non_inf_idxs = np.where(results[:, 2] != np.inf)[0]
-    #iterations = iterations[non_inf_idxs]
-    #results = results[non_inf_idxs, :]
-    max_kde_idx = np.argsort(results[:, 1])[::-1][0:100]
-
-    iters = iterations[max_kde_idx]
-    entropies = results[max_kde_idx, 2]
-    for i, e in zip(iters, entropies):
-        print i, e
-    for i, kde, mse in zip(iterations, results[:, 1], results[:, 0]):
-        print i, kde, mse
-    import pdb;pdb.set_trace()
-
-    print "Max KDE epoch", iterations[max_kde_idx][0]
-    print "Max KDE", results[max_kde_idx, 1][0]
-    print "Max KDE entropy", results[max_kde_idx, 2][0]
-    print "Max KDE min MSE", results[max_kde_idx, 0][0]
-    """
 
 
 if __name__ == '__main__':
