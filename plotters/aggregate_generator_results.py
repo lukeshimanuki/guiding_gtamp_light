ef163d9db920a01010c5ee09fcc1c774d369461d
beomjoon <beomjoon@mit.edu>
2020-05-12 10:14:42 -0400

    cleaning the plotter

--- plotters/aggregate_generator_results.py
+++ plotters/aggregate_generator_results.py
@@ -8,19 +8,12 @@ fdir = './generators/sampler_performances/'
 
 def get_results(fin):
     data = open(fdir + fin, 'r').read().splitlines()
-    result = {'iks': [], 'actions': [], 'pick_mps': [], 'pick_infeasible_mps': [], 'place_mps': [], 'place_infeasible_mps': [], 'success': []}
+    result = {'iks': [], 'actions': [], 'pick_mps': [], 'pick_infeasible_mps': [], 'place_mps': [],
+              'place_infeasible_mps': [], 'success': []}
 
-    raw_dir = './planning_experience/for_testing_generators/'
     for l in data:
         pidx = int(l.split(',')[0])
-        """
-        result_log = "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" % (
-            config.pidx, config.seed, total_ik_checks, total_pick_mp_checks, total_pick_mp_infeasible,
-            total_place_mp_checks, total_place_mp_infeasible, total_mp_checks, total_infeasible_mp,
-            n_total_actions, goal_reached
-        )
-        """
-        if True: #int(l.split(',')[-1]) == 1:
+        if True:  # int(l.split(',')[-1]) == 1:
             result['iks'].append(int(l.split(',')[2]))
             result['pick_mps'].append(int(l.split(',')[3]))
             result['pick_infeasible_mps'].append(int(l.split(',')[4]))
@@ -39,14 +32,13 @@ def print_results(results, result_file):
     print result_file
     keys = ['iks', 'actions', 'pick_mps', 'pick_infeasible_mps', 'place_mps', 'place_infeasible_mps', 'success']
     for key in keys:
-        print "%s %.3f %.3f" % (key, np.mean(results[key]), np.std(results[key])*1.96/np.sqrt(n_data))
+        print "%s %.3f %.3f" % (key, np.mean(results[key]), np.std(results[key]) * 1.96 / np.sqrt(n_data))
+
 
 def average_over_problems(fin):
     raw_dir = './planning_experience/for_testing_generators/'
-    all_plan_exp_files = os.listdir(raw_dir)
     data = open(fdir + fin, 'r').read().splitlines()
 
-    result = {'iks': [], 'actions': [], 'mps': [], 'infeasible_mps': [], 'success': []}
     result = {}
     for l in data:
         pidx = int(l.split(',')[0])
@@ -55,6 +47,7 @@ def average_over_problems(fin):
         else:
             result[pidx] = [int(l.split(',')[9])]
     return result
+
 
 def main():
     file2 = 'phaedra//unif_sqrt_pap_mps_n_mp_limit_5.txt'.format(socket.gethostname())
@@ -68,7 +61,7 @@ def main():
     print_results(results1, file1)
     file1_avg = average_over_problems(file1)
 
-    diff_sorted_idxs = np.argsort([np.mean(file2_avg[k])-np.mean(file1_avg[k]) for k in file1_avg])
+    diff_sorted_idxs = np.argsort([np.mean(file2_avg[k]) - np.mean(file1_avg[k]) for k in file1_avg])
     sorted_keys = np.array(file1_avg.keys())[diff_sorted_idxs]
     for k in sorted_keys:
         """
@@ -81,11 +74,14 @@ def main():
         plan = plan_data['plan']
         plan_length = len(plan)
         """
-        unif_lb = np.mean(file2_avg[k]) - 1.96/np.sqrt(len(file2_avg[k]))*np.std(file2_avg[k])
-        learned_ub = np.mean(file1_avg[k]) + np.std(file1_avg[k])*1.96/np.sqrt(len(file2_avg[k]))
-        unif_ub = np.mean(file2_avg[k]) + 1.96/np.sqrt(len(file2_avg[k]))*np.std(file2_avg[k])
-        learned_lb = np.mean(file1_avg[k]) - np.std(file1_avg[k])*1.96/np.sqrt(len(file2_avg[k]))
-        print "%10d %15.2f+-%.4f %15.2f+-%.4f %15.4f %15.4f %15.4f  better? %2d worse? %2d" % (k, np.mean(file2_avg[k]), 1.96/np.sqrt(len(file2_avg[k]))*np.std(file2_avg[k]), np.mean(file1_avg[k]), np.std(file1_avg[k])*1.96/np.sqrt(len(file2_avg[k])), np.mean(file2_avg[k])-np.mean(file1_avg[k]), unif_lb, learned_ub, unif_lb > learned_ub, learned_lb > unif_ub)
+        unif_lb = np.mean(file2_avg[k]) - 1.96 / np.sqrt(len(file2_avg[k])) * np.std(file2_avg[k])
+        learned_ub = np.mean(file1_avg[k]) + np.std(file1_avg[k]) * 1.96 / np.sqrt(len(file2_avg[k]))
+        unif_ub = np.mean(file2_avg[k]) + 1.96 / np.sqrt(len(file2_avg[k])) * np.std(file2_avg[k])
+        learned_lb = np.mean(file1_avg[k]) - np.std(file1_avg[k]) * 1.96 / np.sqrt(len(file2_avg[k]))
+        print "%10d %15.2f+-%.4f %15.2f+-%.4f %15.4f %15.4f %15.4f  better? %2d worse? %2d" % (
+        k, np.mean(file2_avg[k]), 1.96 / np.sqrt(len(file2_avg[k])) * np.std(file2_avg[k]), np.mean(file1_avg[k]),
+        np.std(file1_avg[k]) * 1.96 / np.sqrt(len(file2_avg[k])), np.mean(file2_avg[k]) - np.mean(file1_avg[k]),
+        unif_lb, learned_ub, unif_lb > learned_ub, learned_lb > unif_ub)
 
 
 if __name__ == '__main__':
