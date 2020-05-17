5e837834a8b5b437e0972dd52d47cbaa34fa250e
Beomjoon Kim <beomjoon@phaedra.csail.mit.edu>
2020-05-17 09:11:06 -0400

    run sahs update

--- test_scripts/multithreaded/run_sahs.py
+++ test_scripts/multithreaded/run_sahs.py
@@ -28,21 +28,23 @@ def main():
     pidxs = [60089, 60061, 60094, 60075, 60074, 60050, 60096, 60057, 60008, 60088, 60026, 60003, 60010, 60067, 60091,
              60031, 60006, 60024, 60030, 60062, 60099, 60018, 60011, 60029, 60098, 60083, 60079, 60016, 60045, 60038,
              60046, 60032, 60058, 60097, 60039]
+    pidx_and_seeds = [(pidx,seed) for pidx in pidxs for seed in range(5)]
+    import pdb;pdb.set_trace()
     configs = []
-    for pidx in pidxs:
-        for planner_seed in range(5):
-            config = {}
-            for k, v in setup._get_kwargs():
-                if type(v) is bool and v is True:
-                    config[k] = ''
-                elif type(v) is not bool:
-                    config[k] = v
-            config['pidx'] = pidx
-            config['planner_seed'] = planner_seed
-            config['timelimit'] = 9999
-            configs.append(config)
+    print "total runs", len(pidxs)*len(range(5))
+    for pidx_seed in pidx_and_seeds:
+        config = {}
+        for k, v in setup._get_kwargs():
+            if type(v) is bool and v is True:
+                config[k] = ''
+            elif type(v) is not bool:
+                config[k] = v
+        config['pidx'] = pidx_seed[0]
+        config['planner_seed'] = pidx_seed[1]
+        config['timelimit'] = 9999
+        configs.append(config)
 
-    n_workers = multiprocessing.cpu_count()
+    n_workers = 1 #multiprocessing.cpu_count()
     pool = ThreadPool(n_workers)
     results = pool.map(worker_wrapper_multi_input, configs)
 
