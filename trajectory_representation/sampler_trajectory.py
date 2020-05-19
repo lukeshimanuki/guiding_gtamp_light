de26dca32b6f232e9aba949b6193a2030ddf899b
beomjoon <beomjoon@mit.edu>
2020-05-19 13:36:01 -0400

    using the new vmanip computation

--- trajectory_representation/sampler_trajectory.py
+++ trajectory_representation/sampler_trajectory.py
@@ -7,6 +7,8 @@ from trajectory_representation.shortest_path_pick_and_place_state import Shortes
 #    count_pickable_goal_objs_and_placeable_to_goal_region_not_yet_in_goal_region
 from planners.subplanners.motion_planner import BaseMotionPlanner
 
+from generators.samplers.sampler import compute_v_manip
+
 import numpy as np
 import random
 import sys
@@ -153,18 +155,17 @@ class SAHSSamplerTrajectory(SamplerTrajectory):
     def __init__(self, problem_idx, n_objs_pack):
         SamplerTrajectory.__init__(self, problem_idx, n_objs_pack)
 
-    def compute_n_in_way(self, action, abs_state, goal_objs):
-        object_moved = action.discrete_parameters['object']
-        target_region = action.discrete_parameters['place_region']
+    def compute_n_in_way_for_object_moved(self, object_moved, abs_state, goal_objs):
 
-        goal_objs_not_in_goal = [goal_obj for goal_obj in goal_objs if
-                                 not abs_state.binary_edges[(goal_obj, 'home_region')][0]
-                                 and goal_obj != object_moved]
+        goal_objs_not_in_goal = [goal_obj for goal_obj in goal_objs
+                                 if not abs_state.binary_edges[(goal_obj, 'home_region')][0]
+                                 and goal_obj != object_moved] # Object can't be in the path to itself
         n_in_way = 0
         for goal_obj in goal_objs_not_in_goal:
             Vpre = abs_state.cached_pick_paths[goal_obj]
             objects_in_way = [o.GetName() for o in self.problem_env.get_objs_in_collision(Vpre, 'entire_region')]
-            n_in_way += object_moved in objects_in_way
+            is_object_moved_in_the_pick_path_to_goal_obj = object_moved in objects_in_way
+            n_in_way += is_object_moved_in_the_pick_path_to_goal_obj
 
         for goal_obj in goal_objs_not_in_goal:
             # Don't I include the pick path collisions?
@@ -172,7 +173,8 @@ class SAHSSamplerTrajectory(SamplerTrajectory):
             pick.execute()
             Vmanip = abs_state.cached_place_paths[(goal_obj, 'home_region')]
             objects_in_way = [o.GetName() for o in self.problem_env.get_objs_in_collision(Vmanip, 'entire_region')]
-            n_in_way += object_moved in objects_in_way
+            is_object_moved_in_the_place_path_for_goal_o_to_r = object_moved in objects_in_way
+            n_in_way += is_object_moved_in_the_place_path_for_goal_o_to_r
             utils.two_arm_place_object(pick.continuous_parameters)
         return n_in_way
 
@@ -202,30 +204,6 @@ class SAHSSamplerTrajectory(SamplerTrajectory):
         }
         return action_info
 
-    def compute_v_manip(self, abs_state, goal_objs):
-        goal_objs_not_in_goal = [goal_obj for goal_obj in goal_objs if
-                                 not abs_state.binary_edges[(goal_obj, 'home_region')][0]]
-        v_manip_values = []
-        v_manip = np.zeros((len(abs_state.prm_vertices), 1))
-        for goal_obj in goal_objs_not_in_goal:
-            prm_path = abs_state.cached_place_paths[(goal_obj, 'home_region')]
-            distances = [utils.base_pose_distance(prm_path[0], prm_vtx) for prm_vtx in abs_state.prm_vertices]
-            closest_prm_idx = np.argmin(distances)
-            prm_path.pop(0)
-            prm_path.insert(0, abs_state.prm_vertices[closest_prm_idx, :])
-
-            distances = [utils.base_pose_distance(prm_path[-1], prm_vtx) for prm_vtx in abs_state.prm_vertices]
-            closest_prm_idx = np.argmin(distances)
-            prm_path[-1] = abs_state.prm_vertices[closest_prm_idx, :]
-
-            v_manip_values.append(prm_path)
-            for p in prm_path:
-                boolean_matching_prm_vertices = np.all(np.isclose(abs_state.prm_vertices[:, :2], p[:2]), axis=-1)
-                if np.any(boolean_matching_prm_vertices):
-                    idx = np.argmax(boolean_matching_prm_vertices)
-                    v_manip[idx] = 1
-        return v_manip
-
     def add_trajectory(self, plan, hvalues):
         print "Problem idx", self.problem_idx
         self.set_seed(self.problem_idx)
@@ -244,22 +222,23 @@ class SAHSSamplerTrajectory(SamplerTrajectory):
             assert action.type == 'two_arm_pick_two_arm_place'
             state = self.compute_state(abs_state, action)
             action_info = self.get_action_info(action)
-            prev_n_in_way = self.compute_n_in_way(action, abs_state, goal_objs)
-            prev_v_manip = self.compute_v_manip(abs_state, goal_objs)
+            prev_n_in_way = self.compute_n_in_way_for_object_moved(action.discrete_parameters['object'],
+                                                                   abs_state, goal_objs)
+            prev_v_manip = compute_v_manip(abs_state, goal_objs)
 
             action.execute_pick()
-            state.place_collision_vector = state.get_collison_vector(None)
+            #state.place_collision_vector = state.convert_collision_at_prm_indices_to_col_vec(abs_state.current_collides)
             action.execute()
 
             print "Computing state..."
             abs_state = ShortestPathPaPState(self.problem_env, goal_entities, parent_state=abs_state,
                                              parent_action=action)
-            v_manip = self.compute_v_manip(abs_state, goal_objs)
-            n_in_way = self.compute_n_in_way(action, abs_state, goal_objs)
+            v_manip = compute_v_manip(abs_state, goal_objs)
+
+            n_in_way = self.compute_n_in_way_for_object_moved(action.discrete_parameters['object'], abs_state, goal_objs)
 
             print action.discrete_parameters['object'], action.discrete_parameters['place_region']
             print 'Prev n in way {} curr n in way {}'.format(prev_n_in_way, n_in_way)
-
             self.add_sah_tuples(state, action_info, prev_n_in_way, n_in_way, prev_v_manip, v_manip)
 
         self.add_state_prime()
