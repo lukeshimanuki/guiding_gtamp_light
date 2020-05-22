cfcc039dd3c9c354af96034b0c3347916aa19e71
beomjoon <beomjoon@mit.edu>
2020-05-22 10:00:49 -0400

    cloud run file updates

--- liscloud_run_sampler.sh
+++ liscloud_run_sampler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NUMBATCHES=1
 for ((BATCHIDX=0;BATCHIDX<1;BATCHIDX++)) do
-    for ((PIDX=60000;PIDX<60100;PIDX++)) do
+    for ((PIDX=60000;PIDX<=60100;PIDX++)) do
         for ((PLANSEED=0;PLANSEED<5;PLANSEED++)) do
               cat liscloud_run_sahs.yaml |
               sed "s/NAME/guiding-gtamp-$BATCHIDX-$PIDX-$PLANSEED/" |
--- liscloud_run_sampler_learn.sh
+++ liscloud_run_sampler_learn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 NUMBATCHES=1
 for ((BATCHIDX=0;BATCHIDX<1;BATCHIDX++)) do
-    for ((PIDX=60000;PIDX<60100;PIDX++)) do
-        for ((PLANSEED=4;PLANSEED<5;PLANSEED++)) do
+    for ((PIDX=60099;PIDX<=60099;PIDX++)) do
+        for ((PLANSEED=2;PLANSEED<=2;PLANSEED++)) do
               cat liscloud_run_sahs_learn.yaml |
               sed "s/NAME/guiding-gtamp-learn-$BATCHIDX-$PIDX-$PLANSEED/" |
               sed "s/PIDX/$PIDX/" |
