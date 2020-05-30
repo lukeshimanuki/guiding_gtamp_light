6033cb09bab0c86ee304b4ee920a938db31c7de8
beomjoon <beomjoon@mit.edu>
2020-05-30 11:50:42 -0400

    cleaning unused code

--- trajectory_representation/concrete_node_state.py
+++ trajectory_representation/concrete_node_state.py
@@ -18,8 +18,6 @@ class ConcreteNodeState:
         self.abs_robot_pose = utils.clean_pose_data(utils.get_body_xytheta(self.problem_env.robot))
         self.abs_obj_pose = utils.clean_pose_data(utils.get_body_xytheta(self.problem_env.env.GetKinBody(self.obj)))
         self.abs_goal_obj_poses = [np.array([0, 0, 0])]
-        #self.abs_goal_obj_poses = [utils.clean_pose_data(utils.get_body_xytheta(o))
-        #                           for o in self.problem_env.goal_entities if 'region' not in o]
 
     def convert_collision_at_prm_indices_to_col_vec(self, obj_pose_and_obj_name_to_prm_indices_in_collision):
         prm_indices_in_collision = list(set.union(*obj_pose_and_obj_name_to_prm_indices_in_collision.values()))
@@ -27,14 +25,5 @@ class ConcreteNodeState:
         collision_vector = np.zeros((len(key_configs)))
         collision_vector[prm_indices_in_collision] = 1
         collision_vector = utils.convert_binary_vec_to_one_hot(collision_vector)
-
-        """
-        # testing if the conversion is working correctly
-        self.problem_env.set_robot_to_default_dof_values()
-        collision_vector2 = utils.compute_occ_vec(key_configs)
-        collision_vector2 = utils.convert_binary_vec_to_one_hot(collision_vector2)
-        print np.sum(np.sum(collision_vector2 == collision_vector,axis=-1)==0)
-        import pdb;pdb.set_trace()
-        """
         collision_vector = collision_vector.reshape((1, len(collision_vector), 2, 1))
         return collision_vector
