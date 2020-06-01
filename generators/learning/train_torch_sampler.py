175d5213fdce98b9138272fc3c1cd5e03a28bfde
beomjoon <beomjoon@mit.edu>
2020-06-01 10:16:32 -0400

    domain option in train sampler

--- generators/learning/train_torch_sampler.py
+++ generators/learning/train_torch_sampler.py
@@ -7,48 +7,51 @@ import pickle
 from datasets.GeneratorDataset import StandardDataset
 from generators.learning.learning_algorithms.WGANGP import WGANgp
 
-if socket.gethostname() == 'lab' or socket.gethostname() == 'phaedra' or socket.gethostname() == 'dell-XPS-15-9560':
-    ROOTDIR = './'
-else:
-    ROOTDIR = '/data/public/rw/pass.port/guiding_gtamp_light/learned_weights/'
+ROOTDIR = './'
 
 
 def save_weights(net, epoch, action_type, seed, region):
     net_name = net.__class__._get_name(net)
     PATH = './learning/torch_weights/atype_%s_%s_region_%s_seed_%d_epoch_%d.pt' % (
-    action_type, net_name, region, seed, epoch)
+        action_type, net_name, region, seed, epoch)
     torch.save(net.state_dict(), PATH)
 
 
-def get_data_generator(action_type, region, seed):
-    dataset = StandardDataset(action_type, region, True, is_testing=False, seed=seed)
+def get_data_generator(config):
+    dataset = StandardDataset(config, True, is_testing=False)
     n_train = int(len(dataset) * 0.9)
     trainset, testset = torch.utils.data.random_split(dataset, [n_train, len(dataset) - n_train])
     batch_size = 32
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=20,
                                               pin_memory=True)
+
+    """
     test_idx_dir = './generators/datasets/testset_cache_file_idxs/'
     if not os.path.isdir(test_idx_dir):
-        os.makedirs(test_idx_dir) 
+        os.makedirs(test_idx_dir)
+    action_type = config.atype
+    region = config.region
+    seed = config.seed
     test_idx_file = 'seed_{}_atype_{}_region_{}.pkl'.format(seed, action_type, region)
-    pickle.dump(testset.indices, open(test_idx_dir+test_idx_file,'wb'))
+    pickle.dump(testset.indices, open(test_idx_dir + test_idx_file, 'wb'))
+    """
     return trainloader, trainset, testset
 
 
 def main():
     parser = argparse.ArgumentParser("Sampler training")
     parser.add_argument('-seed', type=int, default=0)
-    parser.add_argument('-atype', type=str, default='place')
+    parser.add_argument('-atype', type=str, default='pick')
     parser.add_argument('-region', type=str, default='home_region')
+    parser.add_argument('-domain', type=str, default='two_arm_mover')
     parser.add_argument('-architecture', type=str, default='fc')
     config = parser.parse_args()
 
     torch.cuda.manual_seed_all(config.seed)
     torch.manual_seed(config.seed)
+    model = WGANgp(config.atype, config.region, config.architecture, config.seed, config.domain)
 
-    model = WGANgp(config.atype, config.region, config.architecture, config.seed)
-
-    trainloader, trainset, testset = get_data_generator(config.atype, config.region, config.seed)
+    trainloader, trainset, testset = get_data_generator(config)
     n_train = len(trainset)
     model.train(trainloader, testset, n_train)
 
