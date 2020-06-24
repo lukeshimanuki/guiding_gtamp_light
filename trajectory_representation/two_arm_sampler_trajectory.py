c1b4a4437cbe419e6ef8d9746fd5e17084fd443c
beomjoon <beomjoon@mit.edu>
2020-06-24 13:17:30 -0400

    separating rsc and greedy data in trajectory

--- trajectory_representation/two_arm_sampler_trajectory.py
+++ trajectory_representation/two_arm_sampler_trajectory.py
@@ -3,10 +3,8 @@ from trajectory_representation.shortest_path_pick_and_place_state import Shortes
 from generators.samplers.pick_place_learned_sampler import compute_v_manip
 from gtamp_utils import utils
 from trajectory_representation.concrete_node_state import TwoArmConcreteNodeState
-from gtamp_problem_environments.mover_env import PaPMoverEnv
 from test_scripts.run_greedy import get_problem_env, get_goal_obj_and_region
-
-import numpy as np
+import copy
 
 
 class TwoArmSAHSSamplerTrajectory(SamplerTrajectory):
@@ -37,29 +35,83 @@ class TwoArmSAHSSamplerTrajectory(SamplerTrajectory):
         utils.set_robot_config(original_config)
         return n_in_way
 
-    def get_neutral_data(self, nodes, config):
+    # used in RSC data
+    def get_pap_used_in_plan(self, plan):
+        picks = plan[::2]
+        places = plan[1::2]
+        obj_to_pick = {p.discrete_parameters['object']: p for p in picks}
+        obj_to_place = {(p.discrete_parameters['object'], p.discrete_parameters['place_region']): p for p in places}
+        return [obj_to_pick, obj_to_place]
+
+    def compute_state(self, parent_state, parent_action, goal_entities, problem_env):
+        if parent_action is not None:
+            parent_action.discrete_parameters['two_arm_place_object'] = parent_action.discrete_parameters['object']
+        state = ShortestPathPaPState(problem_env, goal_entities, parent_state, parent_action)
+
+        return state
+
+    def delete_moved_objects_from_pap_data(self, pick_used, place_used, moved_obj):
+        moved_obj_name = moved_obj.GetName()
+        new_pick_used = {key: value for key, value in zip(pick_used.keys(), pick_used.values()) if
+                         key != moved_obj_name}
+
+        new_place_used = {}
+        for key, value in zip(place_used.keys(), place_used.values()):
+            if moved_obj_name == key[0]:
+                continue
+            new_place_used[key] = value
+        return new_pick_used, new_place_used
+    # end of used in RSC data
+
+    def get_rsc_data(self, plan, config):
         goal_objs, goal_region = get_goal_obj_and_region(config)
         problem_env = get_problem_env(config, goal_region, goal_objs)
-        neutral_nodes = [n for n in nodes if n.parent is not None and not n.is_goal_traj]
-        neutral_trajs = []
-        for node in neutral_nodes:
-            action = node.parent_action
-            action_info = self.get_action_info(action)
 
-            parent_node = node.parent
-            abs_state = parent_node.state
-            abs_state.problem_env = problem_env
-            state_saver = parent_node.state.state_saver
-            state_saver.Restore()
-            concrete_state = TwoArmConcreteNodeState(abs_state, action)  # does my state have vmanip
-            # todo get the vmanip
+        parent_state = None
+        parent_action = None
 
-            neutral_trajs.append([concrete_state, action_info])
+        paps_used = self.get_pap_used_in_plan(plan)
+        pick_used = paps_used[0]
+        place_used = paps_used[1]
+        goal_entities = goal_objs + [goal_region]
+        openrave_env = problem_env.env
+        all_data = []
+        for action_idx, action in enumerate(plan):
+            if 'place' in action.type:
+                continue
 
-        problem_env.env.Destroy()
-        return neutral_trajs
+            target_obj = openrave_env.GetKinBody(action.discrete_parameters['object'])
 
-    def get_data(self, nodes, config):
+            pick_used, place_used = self.delete_moved_objects_from_pap_data(pick_used, place_used, target_obj)
+
+            action.is_skeleton = False
+            pap_action = copy.deepcopy(action)
+            pap_action = pap_action.merge_operators(plan[action_idx + 1])
+            pap_action.is_skeleton = False
+            pap_action.discrete_parameters['place_region'] = pap_action.discrete_parameters['two_arm_place_place_region'].name
+            import pdb;pdb.set_trace()
+            action_info = self.get_action_info(pap_action)
+
+            abs_state = self.compute_state(parent_state, parent_action, goal_entities, problem_env)
+            v_manip = compute_v_manip(abs_state, goal_objs)
+            concrete_state = TwoArmConcreteNodeState(abs_state, pap_action)
+            data = {"abs_state": abs_state,
+                    "concrete_state": concrete_state,
+                    "action_info": action_info,
+                    "action": action,
+                    "v_manip": v_manip}
+            import pdb;pdb.set_trace()
+
+            pap_action.execute()
+            parent_state = abs_state
+            parent_action = pap_action
+            all_data.append(data)
+            print "Executed", action.discrete_parameters
+
+        openrave_env.Destroy()
+        return data
+
+    def get_greedy_data(self, nodes, config):
         goal_objs, goal_region = get_goal_obj_and_region(config)
         problem_env = get_problem_env(config, goal_region, goal_objs)
         self.problem_env = problem_env
@@ -87,7 +139,8 @@ class TwoArmSAHSSamplerTrajectory(SamplerTrajectory):
             abs_state = node.state
             abs_state.state_saver.Restore()
             v_manip = compute_v_manip(abs_state, goal_objs)
-            n_in_way = self.compute_n_in_way_for_object_moved(action.discrete_parameters['object'], abs_state, goal_objs)
+            n_in_way = self.compute_n_in_way_for_object_moved(action.discrete_parameters['object'], abs_state,
+                                                              goal_objs)
             data = {"abs_state": parent_abs_state,
                     "concrete_state": parent_concrete_state,
                     "action_info": action_info,
