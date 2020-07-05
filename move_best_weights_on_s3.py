c160624b1993b2d38691febf69cead12c1fd0704
root <root@phaedra.csail.mit.edu>
2020-07-05 09:58:46 -0400

    update move best weights

--- move_best_weights_on_s3.py
+++ move_best_weights_on_s3.py
@@ -1,25 +1,43 @@
 import os
 import sys
+import socket
 
 
 def download_from_s3():
-
-    command = 'mc cp csail/bkim/guiding-gtamp/sampler_weights/learned_weights.zip ./ --recursive'
+    command = 'mc cp csail/bkim/guiding-gtamp/sampler_weights/learned_weights_phaedra.zip ./ --recursive'
     print command
     os.system(command)
-    command = 'unzip learned_weights.zip -d ./'
+
+    command = 'unzip learned_weights_phaedra.zip -d ./'
+    print command
+    os.system(command)
+
+    command = 'mc cp csail/bkim/guiding-gtamp/sampler_weights/learned_weights_shakey.zip ./ --recursive'
+    print command
+    os.system(command)
+
+    command = 'unzip learned_weights_shakey.zip -d ./'
     print command
     os.system(command)
 
 
 def send_to_s3(domain):
-    cmd = 'mc rm ./learned_weights.zip'
+    file_name = 'learned_weights_{}.zip'.format(socket.gethostname())
+
+    cmd = 'mc rm ./{}'.format(file_name)
+    print cmd
     os.system(cmd)
-    cmd = 'mc rm csail/bkim/guiding-gtamp/sampler_weights/learned_weights.zip'
+
+    cmd = 'mc rm csail/bkim/guiding-gtamp/sampler_weights/{}'.format(file_name)
+    print cmd
     os.system(cmd)
-    cmd = 'zip -r learned_weights.zip generators/learning/learned_weights'
+
+    cmd = 'zip -r {} generators/learning/learned_weights'.format(file_name)
+    print cmd
     os.system(cmd)
-    cmd = 'mc cp learned_weights.zip csail/bkim/guiding-gtamp/sampler_weights/ --recursive'
+
+    cmd = 'mc cp {} csail/bkim/guiding-gtamp/sampler_weights/ --recursive'.format(file_name)
+    print cmd
     os.system(cmd)
     return
 
