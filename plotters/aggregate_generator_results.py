880db25d1d473571ab4c75e44a5b1c178f7a3eaf
root <root@phaedra.csail.mit.edu>
2020-07-16 16:49:48 -0400

    updating aggregate generator

--- plotters/aggregate_generator_results.py
+++ plotters/aggregate_generator_results.py
@@ -6,21 +6,20 @@ import pickle
 fdir = './generators/sampler_performances/'
 
 
-def get_results(fin):
-    data = open(fdir + fin, 'r').read().splitlines()
+def get_results(fin_list):
     result = {'iks': [], 'actions': [], 'pick_mps': [], 'pick_infeasible_mps': [], 'place_mps': [],
               'place_infeasible_mps': [], 'success': []}
+    target_pidxs = [4, 3, 14, 2, 17, 7, 23, 11, 9, 19, 21, 16, 1, 10, 12, 18, 24, 15]
+    target_pidxs = [4, 3, 14, 2, 17, 7, 23, 10, 12, 18, 24, 15]
 
-    pidxs = [60053, 60023, 60081, 60001, 60021, 60008, 60062, 60079, 60033, 60044, 60031, 60018, 60075, 60050, 60030, 60020, 60098, 60016, 60067, 60061, 60024, 60096, 60005, 60088, 60091, 60010, 60011, 60045, 60006, 60099, 60038, 60083, 60058, 60046, 60029, 60032, 60097, 60039]
-    pidxs = [60089, 60061, 60094, 60075, 60074, 60050, 60096, 60057, 60008, 60088, 60026, 60003, 60010, 60067, 60091,
-             60031, 60006, 60024, 60030, 60062, 60099, 60018, 60011, 60029, 60098, 60083, 60079, 60016, 60045, 60038,
-             60046, 60032, 60058, 60097, 60039]
 
-    for l in data:
-        pidx = int(l.split(',')[0])
-        #if True:  # int(l.split(',')[-1]) == 1:
-        #if 60000<= pidx < 60100:
-        if pidx in pidxs:
+    for fin in fin_list:
+        data = open(fdir + fin, 'r').read().splitlines()
+
+        for l in data:
+            pidx = int(l.split(',')[0])
+            #if pidx not in target_pidxs:
+            #    continue
             result['iks'].append(int(l.split(',')[2]))
             result['pick_mps'].append(int(l.split(',')[3]))
             result['pick_infeasible_mps'].append(int(l.split(',')[4]))
@@ -43,60 +42,58 @@ def print_results(results, result_file):
         print "%s %.3f %.3f" % (key, np.mean(results[key]), np.std(results[key]) * 1.96 / np.sqrt(n_data))
 
 
-def average_over_problems(fin):
+def average_over_problems(fin_list):
     raw_dir = './planning_experience/for_testing_generators/'
-    data = open(fdir + fin, 'r').read().splitlines()
 
     result = {}
-    for l in data:
-        pidx = int(l.split(',')[0])
-        if pidx in result:
-            result[pidx].append(int(l.split(',')[9]))
-        else:
-            result[pidx] = [int(l.split(',')[9])]
+    for fin in fin_list:
+        data = open(fdir + fin, 'r').read().splitlines()
+
+        for l in data:
+            pidx = int(l.split(',')[0])
+            if pidx in result:
+                result[pidx].append(int(l.split(',')[9]))
+            else:
+                result[pidx] = [int(l.split(',')[9])]
     return result
 
 
 def main():
-    file2 = 'phaedra//uniform_sqrt_pap_mps_n_mp_limit_5.txt'.format(socket.gethostname())
-    #file2 = 'phaedra//uniform_sqrt_pap_mps_n_mp_limit_5.txt'
-    results2 = get_results(file2)
-    print_results(results2, file2)
-    file2_avg = average_over_problems(file2)
+    unif_file = 'phaedra//uniform.txt'.format(socket.gethostname())
+    unif_results = get_results([unif_file])
+    print_results(unif_results, unif_file)
 
     print '============================================================'
-    file1 = 'phaedra/pap_pick_fc_place_fc.txt'.format(socket.gethostname())
-    results1 = get_results(file1)
-    print_results(results1, file1)
-    file1_avg = average_over_problems(file1)
+    file_list = ['phaedra/wgandi_sampler_seed_0.txt', 
+                 'phaedra/wgandi_sampler_seed_1.txt',\
+                 'phaedra/wgandi_sampler_seed_2.txt',\
+                # 'phaedra/wgandi_sampler_seed_3.txt',
+                ]
+    results = get_results(file_list)
+    print_results(results, file_list)
 
-    diff_sorted_idxs = np.argsort([np.mean(file2_avg[k]) - np.mean(file1_avg[k]) for k in file1_avg])
-    sorted_keys = np.array(file1_avg.keys())[diff_sorted_idxs]
-    print 'former:',file2
-    print 'latter:',file1
+    smpler_avg = average_over_problems(file_list)
+    unif_avg = average_over_problems([unif_file])
+  
+    diff_sorted_idxs = np.argsort([np.mean(smpler_avg[k]) - np.mean(unif_avg[k]) for k in unif_avg])
+    sorted_keys = np.array(unif_avg.keys())[diff_sorted_idxs]
+    print 'former:',unif_file
+    print 'latter:',file_list
     hard_keys = []
+    print "{} {} {} {}".format( "PIDX", "Smpler", "unif", "Smpler-unif")
     for k in sorted_keys:
-        """
-        raw_dir = './planning_experience/for_testing_generators/'
-        fname = 'pidx_%d_planner_seed_0_gnn_seed_0.pkl' % k
-        try:
-            plan_data = pickle.load(open(raw_dir + fname, 'r'))
-        except:
-            plan_data = pickle.load(open(raw_dir+'sampling_strategy_uniform'+fname,'r'))
-        plan = plan_data['plan']
-        plan_length = len(plan)
-        """
-        if np.mean(file2_avg[k]) - np.mean(file1_avg[k]) > 5:
-            unif_lb = np.mean(file2_avg[k]) - 1.96 / np.sqrt(len(file2_avg[k])) * np.std(file2_avg[k])
-            learned_ub = np.mean(file1_avg[k]) + np.std(file1_avg[k]) * 1.96 / np.sqrt(len(file2_avg[k]))
-            unif_ub = np.mean(file2_avg[k]) + 1.96 / np.sqrt(len(file2_avg[k])) * np.std(file2_avg[k])
-            learned_lb = np.mean(file1_avg[k]) - np.std(file1_avg[k]) * 1.96 / np.sqrt(len(file2_avg[k]))
-            print "%10d %15.2f+-%.4f %15.2f+-%.4f %15.4f %15.4f %15.4f  better? %2d worse? %2d" % (
+        if abs(np.mean(smpler_avg[k]) - np.mean(unif_avg[k])) > 10:
+            #unif_lb = np.mean(smpler_avg[k]) - 1.96 / np.sqrt(len(smpler_avg[k])) * np.std(smpler_avg[k])
+            #learned_ub = np.mean(unif_avg[k]) + np.std(unif_avg[k]) * 1.96 / np.sqrt(len(smpler_avg[k]))
+            #unif_ub = np.mean(smpler_avg[k]) + 1.96 / np.sqrt(len(smpler_avg[k])) * np.std(smpler_avg[k])
+            #learned_lb = np.mean(unif_avg[k]) - np.std(unif_avg[k]) * 1.96 / np.sqrt(len(smpler_avg[k]))
+            print "%10d %15.2f+-%.4f %15.2f+-%.4f %15.4f " % (
             k, 
-            np.mean(file2_avg[k]), 1.96 / np.sqrt(len(file2_avg[k])) * np.std(file2_avg[k]), 
-            np.mean(file1_avg[k]), np.std(file1_avg[k]) * 1.96 / np.sqrt(len(file2_avg[k])), 
-            np.mean(file2_avg[k]) - np.mean(file1_avg[k]),
-            unif_lb, learned_ub, unif_lb > learned_ub, learned_lb > unif_ub)
+            np.mean(smpler_avg[k]), 1.96 / np.sqrt(len(smpler_avg[k])) * np.std(smpler_avg[k]), 
+            np.mean(unif_avg[k]), np.std(unif_avg[k]) * 1.96 / np.sqrt(len(smpler_avg[k])), 
+            np.mean(smpler_avg[k]) - np.mean(unif_avg[k]),
+           # unif_lb, learned_ub, unif_lb > learned_ub, learned_lb > unif_ub
+            )
             hard_keys.append(k)
     print hard_keys
           
