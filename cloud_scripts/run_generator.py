50a940ae4c7416dc9efb2ec6b3e641dbda59dd23
beomjoon <beomjoon@mit.edu>
2020-07-17 11:17:40 -0400

    multithreaded runs using given sampler seed idx and epoch idx

---
+++ cloud_scripts/run_generator.py
@@ -0,0 +1,122 @@
+import os
+import subprocess
+import re
+import time
+
+
+def filter_runs_already_done(undone, dir):
+    # filter things already one
+    if os.path.isdir(dir):
+        files = os.listdir(dir)
+        for file in files:
+            seed = int(file.split('_')[1])
+            pidx = int(file.split('_')[3].split('.pkl')[0])
+            undone.remove((seed, pidx))
+    return undone
+
+
+def get_yaml_file_name(algorithm, domain):
+    yaml_file = 'run_gather_planning_exp.yaml'
+    return yaml_file
+
+
+def get_s3_path(commithash):
+    s3_path = 'csail/bkim/guiding-gtamp/planning_experience/' \
+              '{}/' \
+              'raw/two_arm_mover' \
+              'n_objs_pack_1/' \
+              'hcount_old_number_in_goal/' \
+              'q_config_num_train_5000_mse_weight_1.0_use_region_agnostic_False_mix_rate_1.0/' \
+              'n_mp_limit_5_n_iter_limit_2000/'.format(commithash[0:7])
+    return s3_path
+
+
+def get_done_seed_and_pidx_pairs(commithash):
+    s3_path = get_s3_path(commithash)
+    try:
+        result = subprocess.check_output('mc ls {}'.format(s3_path), shell=True)
+    except subprocess.CalledProcessError:
+        return []
+
+    runs_finished = re.findall('pidx_[0-9]*_planner_seed_[0-9]*_gnn_seed_[0-9]*', result)
+    if 'sampling_strategy' in runs_finished[0]:
+        seed_pidx_pairs_finished = [{'pidx': fin.split('_')[1], 'seed': fin.split('_')[-1]} for fin in runs_finished]
+    else:
+        seed_pidx_pairs_finished = [{'pidx': fin.split('_')[1], 'seed': fin.split('_')[4]} for fin in runs_finished]
+
+    return seed_pidx_pairs_finished
+
+
+def get_seed_and_pidx_pairs_that_needs_to_run(pidxs, seed_pidx_pairs_finished):
+    undone = [(seed, pidx) for seed in range(1) for pidx in pidxs]
+    done = [(int(k['seed']), int(k['pidx'])) for k in seed_pidx_pairs_finished]
+    undone = [un for un in undone if un not in done]
+    return undone
+
+
+def get_running_seed_and_pidx_pairs(algorithm, domain):
+    cmd = 'kubectl get jobs $(kubectl get jobs -o=jsonpath=\'{.items[?(@.status.running>0)].metadata.name}\') -n beomjoon'
+    results = subprocess.check_output(cmd, shell=True).split('\n')
+    running = []
+    for result in results:
+        if domain not in result:
+            continue
+        if algorithm not in result:
+            continue
+        pidx = int(result.split('-')[5])
+        seed = int(result.split('-')[6].split(' ')[0])
+        running.append({'pidx': pidx, 'seed': seed})
+    return running
+
+
+def main():
+    domain = 'two_arm_mover'
+    hoption = 'hcount_old_number_in_goal'
+    algorithm = 'greedy-hcount'
+
+    timelimit = 2000
+    n_iter_limit = 2000
+    n_objs_pack = 1
+    absq_seed = 0
+
+    if domain == 'two_arm_mover':
+        target_pidxs = range(41000, 42000)
+    else:
+        target_pidxs = range(1000)
+
+    yaml_file = get_yaml_file_name(algorithm, domain)
+    commithash = '669634c699ac3f0a4628feae4d489fa226504039'
+
+    seed_pidx_pairs_running = []# get_running_seed_and_pidx_pairs(domain, algorithm
+    seed_pidx_pairs_finished = []
+    undone = get_seed_and_pidx_pairs_that_needs_to_run(target_pidxs, seed_pidx_pairs_finished + seed_pidx_pairs_running)
+    print "Remaining runs", len(undone)
+    consecutive_runs = 0
+    algo = 'greedy'
+    for n_objs_pack in [1]:
+        for idx, un in enumerate(undone):
+            pidx = un[1]
+            if algo == 'rsc':
+                yaml_file = 'run_gather_planning_exp.yaml'
+            else:
+                yaml_file = 'run_gather_sampler_planning_exp.yaml'
+
+            cmd = 'cat cloud_scripts/{} | ' \
+                  'sed \"s/NAME/plan-exp-{}-{}/\" |  ' \
+                  'sed \"s/PIDX/{}/\" |  ' \
+                  'sed \"s/COMMITHASH/{}/\" |  ' \
+                  'sed \"s/NOBJSPACK/{}/\" |  ' \
+                  'sed \"s/DOMAIN/{}/\" |  ' \
+                  'kubectl apply -f - -n beomjoon;'.format(yaml_file, pidx, n_objs_pack, pidx, commithash, n_objs_pack, domain)
+            print idx, cmd
+            os.system(cmd)
+            import pdb;pdb.set_trace()
+            time.sleep(2)
+            consecutive_runs += 1
+            if consecutive_runs % 100 == 0:
+                print "Long break"
+                time.sleep(30)
+
+
+if __name__ == '__main__':
+    main()
--- test_scripts/multithreaded/run_generator.py
+++ test_scripts/multithreaded/run_generator.py
@@ -7,7 +7,7 @@ import numpy as np
 from multiprocessing.pool import ThreadPool  # dummy is nothing but multiprocessing but wrapper around threading
 from threaded_test_utils import get_sahs_configs
 from test_scripts.run_generator import get_logfile_name, parse_arguments
-from test_scripts.run_greedy import get_seed_and_epochs
+from test_scripts.run_generator import get_seed_and_epochs
 
 
 def worker_p(config):
@@ -24,45 +24,35 @@ def worker_wrapper_multi_input(multi_args):
     return worker_p(multi_args)
 
 
-def main():
-    target_pidxs = [40064, 40071, 40077, 40078, 40080, 40083, 40088, 40097, 40098, 40003, 40007, 40012, 40018,
-                    40020, 40023, 40030, 40032, 40033, 40036, 40038, 40047, 40055, 40059, 40060, 40062]
-    target_pidxs = target_pidxs[10:]
-
-    target_pidx_idxs = range(len(target_pidxs))
-
-    setup = parse_arguments()
-    sampler_seeds = range(3)
-    planner_seeds = range(4)
+def get_all_configs(target_pidx_idxs, setup):
+    if 'pick' in setup.learned_sampler_atype:
+        _, _, total_epochs = get_seed_and_epochs('pick', '', setup)
+    else:
+        region = setup.learned_sampler_atype.split('_')[1]
+        _, _, total_epochs = get_seed_and_epochs('place', region + '_region', setup)
+    total_epochs = range(len(total_epochs))
     configs = []
+    for epoch in total_epochs:
+        for idx in target_pidx_idxs:
+            config = {}
+            for k, v in setup._get_kwargs():
+                if type(v) is bool and v is True:
+                    config[k] = ''
+                elif type(v) is not bool:
+                    config[k] = v
+            config['sampler_epoch_idx'] = epoch
+            config['target_pidx_idx'] = idx
+            configs.append(config)
+    return configs
 
-    sampler_seeds = [0]
-    action_types = ['place_loading']
-    for action_type in action_types:
-        for planner_seed in planner_seeds:
-            for sampler_seed in sampler_seeds:
-                setup.sampler_seed = sampler_seed
-                if 'pick' in action_type:
-                    _, _, total_epochs = get_seed_and_epochs('pick', '', setup)
-                else:
-                    region = action_type.split('_')[1]
-                    _, _, total_epochs, _= get_seed_and_epochs('place', region+'_region', setup)
-                total_epochs = range(len(total_epochs))
-                total_epochs = [439]
-                for epoch in total_epochs:
-                    for idx in target_pidx_idxs:
-                        config = {}
-                        for k, v in setup._get_kwargs():
-                            if type(v) is bool and v is True:
-                                config[k] = ''
-                            elif type(v) is not bool:
-                                config[k] = v
-                        config['sampler_epoch'] = epoch
-                        config['sampler_seed'] = sampler_seed
-                        config['target_pidx_idx'] = idx
-                        config['learned_sampler_atype'] = action_type
-                        config['planner_seed'] = planner_seed
-                        configs.append(config)
+
+def main():
+    target_pidxs = range(40200, 40210)
+    target_pidx_idxs = range(len(target_pidxs))
+    setup = parse_arguments()
+
+    configs = get_all_configs(target_pidx_idxs, setup)
+
     n_workers = multiprocessing.cpu_count()
     pool = ThreadPool(n_workers)
     results = pool.map(worker_wrapper_multi_input, configs)
