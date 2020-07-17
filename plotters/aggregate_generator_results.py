eb6a40bcdbb8392d95cb0f3472c97bd210691677
root <root@phaedra.csail.mit.edu>
2020-07-17 14:33:27 -0400

    updating aggregate generator

--- plotters/aggregate_generator_results.py
+++ plotters/aggregate_generator_results.py
@@ -3,7 +3,7 @@ import os
 import socket
 import pickle
 import collections
-from test_scripts.run_greedy import get_seed_and_epochs
+#from test_scripts.run_generator import convert_seed_epoch_idxs_to_seed_and_epoch
 
 fdir = './generators/sampler_performances/'
 
@@ -11,11 +11,7 @@ fdir = './generators/sampler_performances/'
 def get_results(fin_list):
     result = {'iks': [], 'actions': [], 'pick_mps': [], 'pick_infeasible_mps': [], 'place_mps': [],
               'place_infeasible_mps': [], 'success': []}
-    target_pidxs = [4, 3, 14, 2, 17, 7, 23, 11, 9, 19, 21, 16, 1, 10, 12, 18, 24, 15]
-    target_pidxs = [4, 3, 14, 2, 17, 7, 23, 10, 12, 18, 24, 15]
-    target_pidxs = [40064, 40071, 40077, 40078, 40080, 40083, 40088, 40097, 40098, 40003, 40007, 40012, 40018,
-                    40020, 40023, 40030, 40032, 40033, 40036, 40038, 40047, 40055, 40059, 40060, 40062]
-    target_pidxs = range(10)
+    target_pidxs = range(9)
 
 
     for fin in fin_list:
@@ -24,8 +20,8 @@ def get_results(fin_list):
         for l in data:
             pidx = int(l.split(',')[0])
   
-            if pidx not in target_pidxs:
-                continue
+            #if pidx not in target_pidxs:
+            #    continue
             result['iks'].append(int(l.split(',')[2]))
             result['pick_mps'].append(int(l.split(',')[3]))
             result['pick_infeasible_mps'].append(int(l.split(',')[4]))
@@ -67,58 +63,31 @@ def average_over_problems(fin_list):
 
 
 def main():
-    unif_file = 'phaedra//uniform.txt'.format(socket.gethostname())
+    unif_file = 'phaedra//pick_place_home_place_loading//uniform.txt'.format(socket.gethostname())
     unif_results = get_results([unif_file])
     print_results(unif_results, unif_file)
 
-    old_file = 'phaedra//using_200_data_points/wgandi_sampler_seed_0.txt'.format(socket.gethostname())
-    old_results = get_results([old_file])
-    print_results(old_results, old_file)
-
-    best_file = 'phaedra/place_loading/wgandi_sampler_seed_0_epoch_439.txt'.format(socket.gethostname())
-    best_results = get_results([best_file])
-    print_results(best_results, best_file)
-    import pdb;pdb.set_trace()
-
     print '============================================================'
-    # place loading seed dir order: [4, 3, 12, 5]
 
-    config_type = collections.namedtuple('type', 'domain num_episode train_type sampler_seed sampler_epoch')
-
-    file_list = ['/phaedra/place_loading/' + f for f in os.listdir('generators/sampler_performances/phaedra/place_loading/')]
+    file_list = ['/phaedra/place_loading/sampler_seed_4/wgandi/' + f for f in os.listdir('generators/sampler_performances/phaedra/place_loading/sampler_seed_4/wgandi/')]
     epoch_n_actions =  []
 
-    target_seed = 0
-    config = config_type(
-          domain = 'two_arm_mover',
-          num_episode = 1000,
-          train_type = 'wgandi',
-          sampler_seed = target_seed,
-          sampler_epoch = 0)
-    _, _, seeds, epochs = get_seed_and_epochs('place', 'loading_region', config)
 
     for f in file_list:
       epoch = int(f.split('epoch_')[1].split('.txt')[0])
-      seed = int(f.split('seed_')[1].split('_')[0])
-      if seed != 0:
-          continue
       results = get_results([f])
       iks = results['iks']
       n_data = len(iks)
-      if n_data < 10:
+      if n_data < 1:
           continue
       print_results(results, [f])
-      
-      true_seed = seeds[seed]
-      true_epoch = epochs[epoch]
-      epoch_n_actions.append([true_epoch, np.mean(results['actions']), epoch])
+      epoch_n_actions.append([epoch, np.mean(results['actions'])])
 
     epoch_n_actions = np.array(epoch_n_actions)
     epoch_n_actions = epoch_n_actions[np.argsort(epoch_n_actions[:, 1]),:]
     best_epoch = epoch_n_actions[np.argmin(epoch_n_actions[:, 1]), 0]
-    epoch_idx = epoch_n_actions[np.argmin(epoch_n_actions[:, 1]), 2]
     n_actions = epoch_n_actions[np.argmin(epoch_n_actions[:,1]),1]
-    print "Best epoch for seed {} is {} with {} number of actions".format(true_seed, best_epoch, n_actions)
+    print "Best epoch is {} with {} number of actions".format(best_epoch, n_actions)
     print "Uniform {} number of actions".format(np.mean(unif_results['actions']))
     import pdb;pdb.set_trace()
 
