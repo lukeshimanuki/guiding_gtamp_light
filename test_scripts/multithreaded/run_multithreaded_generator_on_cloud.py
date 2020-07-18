964dbd20996d25afcc4447926e0827d39f92c315
beomjoon <beomjoon@mit.edu>
2020-07-18 08:39:32 -0400

    test7

--- test_scripts/multithreaded/run_multithreaded_generator_on_cloud.py
+++ test_scripts/multithreaded/run_multithreaded_generator_on_cloud.py
@@ -5,11 +5,11 @@ import time
 cmd = 'python upload_generator_results.py &'
 os.system(cmd)
 print "DID I RUN THIS?"
-"""
 generator_commands = ''
 for arg in sys.argv[1:]:
     generator_commands += ' ' + arg
 
 generator_commands = 'python test_scripts/multithreaded/run_generator.py ' + generator_commands
 os.system(generator_commands)
-"""
+
+os.system('ps -t')
