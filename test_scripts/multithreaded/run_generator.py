7ca3c48317021573a6deaafd24882a7d0b64cfab
beomjoon <beomjoon@mit.edu>
2020-07-18 08:10:28 -0400

    testing run generator

--- test_scripts/multithreaded/run_generator.py
+++ test_scripts/multithreaded/run_generator.py
@@ -1,30 +1,19 @@
-import os
 import multiprocessing
-import argparse
-import socket
-import numpy as np
-
-from multiprocessing.pool import ThreadPool  # dummy is nothing but multiprocessing but wrapper around threading
-from threaded_test_utils import get_sahs_configs
-from test_scripts.run_generator import get_logfile_name, parse_arguments
-from test_scripts.run_generator import convert_seed_epoch_idxs_to_seed_and_epoch
 import time
+from multiprocessing.pool import ThreadPool  # dummy is nothing but multiprocessing but wrapper around threading
+from test_scripts.run_generator import parse_arguments
+from test_scripts.run_generator import convert_seed_epoch_idxs_to_seed_and_epoch
+
 
 def worker_p(config):
-    if 'do_uploading' in config:
-        print "Running upload generator results"
-        cmd = 'python upload_generator_results.py'
-        print cmd
-        os.system(cmd)
-    else:
-        command = 'python ./test_scripts/run_generator.py'
+    command = 'python ./test_scripts/run_generator.py'
 
-        for key, value in zip(config.keys(), config.values()):
-            option = ' -' + str(key) + ' ' + str(value)
-            command += option
-        print command
-    #time.sleep(10)
-    #os.system(command)
+    for key, value in zip(config.keys(), config.values()):
+        option = ' -' + str(key) + ' ' + str(value)
+        command += option
+    print command
+    time.sleep(10)
+    # os.system(command)
 
 
 def worker_wrapper_multi_input(multi_args):
@@ -41,7 +30,7 @@ def get_all_configs(target_pidx_idxs, setup):
         total_epochs = range(len(total_epochs))
     else:
         total_epochs = [0]
-        
+
     configs = []
     print "Total number of epochs", len(total_epochs)
     for epoch in total_epochs:
@@ -59,18 +48,18 @@ def get_all_configs(target_pidx_idxs, setup):
 
 
 def main():
-    target_pidxs = [40200,40201,40202,40204,40205,40206,40207,40208,40209]
+    target_pidxs = [40200, 40201, 40202, 40204, 40205, 40206, 40207, 40208, 40209]
     target_pidx_idxs = range(len(target_pidxs))
     setup = parse_arguments()
 
-    #configs = get_all_configs(target_pidx_idxs, setup)
-    configs = [{'a':'b'}]*10 + [{'do_uploading'}]
+    # configs = get_all_configs(target_pidx_idxs, setup)
+    configs = [{'a': 'b'}] * 10
     n_workers = multiprocessing.cpu_count()
     pool = ThreadPool(n_workers)
     results = pool.map(worker_wrapper_multi_input, configs)
     pool.close()
     pool.join()
-    #print results
+    # print results
 
 
 if __name__ == '__main__':
