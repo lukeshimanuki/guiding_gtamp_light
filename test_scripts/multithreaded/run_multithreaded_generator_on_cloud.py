939566848625c1ef904ba3ece498b3ae396efc87
beomjoon <beomjoon@mit.edu>
2020-07-18 08:25:36 -0400

    removing run generator part

--- test_scripts/multithreaded/run_multithreaded_generator_on_cloud.py
+++ test_scripts/multithreaded/run_multithreaded_generator_on_cloud.py
@@ -4,10 +4,11 @@ from test_scripts.run_generator import parse_arguments
 
 cmd = 'python upload_generator_results.py &'
 os.system(cmd)
-
+"""
 generator_commands = ''
 for arg in sys.argv[1:]:
     generator_commands += ' ' + arg
 
 generator_commands = 'python test_scripts/multithreaded/run_generator.py ' + generator_commands
 os.system(generator_commands)
+"""
