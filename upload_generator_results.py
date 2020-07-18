6237b8f2c989d1db501dbdb9fa303a913195c933
beomjoon <beomjoon@mit.edu>
2020-07-18 08:34:47 -0400

    test4

--- upload_generator_results.py
+++ upload_generator_results.py
@@ -4,16 +4,15 @@ import time
 
 print "Inside the upload generator"
 
-while True:
-    print "inside the loop for uploading files"
-    time.sleep(600)
+print "inside the loop for uploading files"
+#time.sleep(600)
 
-    file_name = 'sampler_performances_{}.zip'.format(socket.gethostname())
-    cmd = 'zip -r -qq {} generators/sampler_performances'.format(file_name)
-    os.system(cmd)
+file_name = 'sampler_performances_{}.zip'.format(socket.gethostname())
+cmd = 'zip -r -qq {} generators/sampler_performances'.format(file_name)
+os.system(cmd)
 
-    cmd = 'mc rm csail/bkim/guiding-gtamp/{}'.format(file_name)
-    os.system(cmd)
+cmd = 'mc rm csail/bkim/guiding-gtamp/{}'.format(file_name)
+os.system(cmd)
 
-    cmd = 'mc cp {} csail/bkim/guiding-gtamp/ --recursive'.format(file_name)
-    os.system(cmd)
+cmd = 'mc cp {} csail/bkim/guiding-gtamp/ --recursive'.format(file_name)
+os.system(cmd)
