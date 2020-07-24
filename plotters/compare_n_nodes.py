3a1cdaad79125f9292efa01c9d8519d6e5e11a09
beomjoon <beomjoon@mit.edu>
2020-07-24 12:16:58 -0400

    update in compare n nodes

--- plotters/compare_n_nodes.py
+++ plotters/compare_n_nodes.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 
 
-def get_n_nodes(target_dir):
+def get_n_nodes(target_dir, is_valid_idxs):
     test_files = os.listdir(target_dir)
     n_iks = []
     n_nodes = []
@@ -17,44 +17,17 @@ def get_n_nodes(target_dir):
                            'pkl' in filename]
     test_files = np.array(test_files)[np.argsort(test_file_pidxs)]
     if 'two_arm' in target_dir:
-        if 'n_objs_pack_1' in target_dir:
-            # if 'pure_learning' not in target_dir:
-            #    assert '934adde' in target_dir, 'n objs pack for two arm must use commit 934adde'
+        if is_valid_idxs:
             target_pidxs = [40200, 40201, 40202, 40204, 40205, 40206, 40207, 40208, 40209]
-            # target_pidxs = [40064, 40071, 40077, 40078, 40080, 40083, 40088, 40097, 40098, 40003, 40007, 40012, 40018, 40020, 40023, 40030, 40032, 40033, 40036, 40038, 40047, 40055, 40059, 40060, 40062]
-            # target_pidxs = range(40000, 40100)
-
         else:
-            target_pidxs = [40321, 40203, 40338, 40089, 40220, 40223, 40352, 40357, 40380, 40253, 40331, 40260, 40353,
-                            40393, 40272, 40148, 40149, 40283, 40162, 40292, 40295, 40185, 40314, 40060]
-            target_pidxs = range(40000, 40300)
-            target_pidxs = [40000, 40002, 40003, 40004, 40005, 40007, 40008, 40010, 40012, 40014, 40015, 40016, 40017,
-                            40019, 40021, 40023, 40024, 40025, 40026, 40028, 40030, 40031, 40033, 40035, 40036, 40037,
-                            40038, 40039, 40040, 40044, 40045, 40046, 40049, 40050, 40053, 40054, 40055, 40056, 40057,
-                            40060, 40061, 40062, 40064, 40066, 40069, 40073, 40074, 40076, 40078, 40083, 40084, 40085,
-                            40086, 40089, 40093, 40094, 40095, 40097, 40099, 40100, 40104, 40105, 40108, 40109, 40110,
-                            40112, 40117, 40119, 40120, 40124, 40128, 40133, 40135, 40138, 40141, 40142, 40143, 40144,
-                            40146, 40147, 40148, 40150, 40151, 40154, 40159, 40163, 40165, 40166, 40167, 40170, 40174,
-                            40175, 40176, 40177, 40178, 40180, 40182, 40184, 40185, 40186, 40187, 40190, 40191, 40196,
-                            40198, 40202, 40205, 40206, 40208, 40209, 40210, 40212, 40217, 40218, 40220, 40221, 40223,
-                            40227, 40228, 40230, 40232, 40233, 40234, 40238, 40240, 40243, 40244, 40246, 40247, 40249,
-                            40251, 40252, 40258, 40262, 40264, 40265, 40267, 40268, 40269, 40271, 40274, 40275, 40276,
-                            40281, 40283, 40284, 40285, 40286, 40287, 40288, 40290, 40291, 40292, 40294, 40295, 40296,
-                            40297, 40298]
-            target_pidxs = [40000, 40002, 40003, 40004, 40005, 40007, 40008, 40010, 40012, 40014, 40015, 40016, 40017,
-                            40019, 40021, 40023, 40024, 40025, 40026, 40028, 40030, 40031, 40033, 40035, 40036, 40037,
-                            40038, 40039, 40040, 40044, 40045, 40046, 40049, 40050, 40053, 40054, 40055, 40056, 40057,
-                            40060, 40061, 40062, 40064, 40066, 40069, 40073, 40074, 40076, 40078, 40083, 40084, 40085,
-                            40086, 40089, 40093, 40094, 40095, 40097, 40099, 40100, 40104, 40105, 40108, 40109, 40110,
-                            40112, 40117, 40119, 40120, 40124, 40128, 40133, 40135, 40138, 40141, 40142, 40143, 40144,
-                            40146, 40147, 40148, 40150, 40151, 40154, 40159, 40163, 40165, 40166, 40167, 40170, 40174,
-                            40175, 40176, 40177, 40178, 40180, 40182, 40184, 40185, 40186, 40187, 40190, 40191, 40196,
-                            40198, 40202, 40205, 40206, 40208, 40209, 40210, 40212, 40217, 40218, 40220, 40221, 40223,
-                            40227, 40228, 40230, 40232, 40233, 40234, 40238, 40240, 40243, 40244, 40246, 40247, 40249,
-                            40251, 40252, 40258, 40262, 40264, 40265, 40267, 40268, 40269, 40271, 40274, 40275, 40276,
-                            40281, 40283, 40284, 40285, 40286, 40287, 40288, 40290, 40291, 40292, 40294, 40295, 40296,
-                            40297, 40298]
-            target_pidxs = target_pidxs[0:25]
+            if 'n_objs_pack_1' in target_dir:
+                target_pidxs = [40064, 40071, 40077, 40078, 40080, 40083, 40088, 40097, 40098, 40003, 40007, 40012,
+                                40018,
+                                40020, 40023, 40030, 40032, 40033, 40036, 40038, 40047, 40055, 40059, 40060, 40062]
+            else:
+                target_pidxs = [40000, 40002, 40003, 40004, 40005, 40007, 40008, 40010, 40012, 40014, 40015, 40016,
+                                40017,
+                                40019, 40021, 40023, 40024, 40025, 40026, 40028, 40030, 40031, 40033, 40035, 40036]
     else:
         target_pidxs = [20001, 20002, 20003, 20004, 20008, 20009, 20011, 20015, 20019, 20021, 20023, 20024, 20035,
                         20046, 20047, 20051, 20053, 20056, 20057, 20061, 20063, 20066, 20067, 20069, 20072, 20075,
@@ -121,12 +94,17 @@ def get_n_nodes(target_dir):
         else:
             timelimit = 1000
 
+        """
         if fin['tottime'] >= timelimit:
             successes.append(False)
             timetaken = timelimit
         else:
             successes.append(fin['success'])
             timetaken = fin['tottime']
+        """
+        successes.append(fin['success'])
+        timetaken = fin['tottime']
+
 
         times.append(timetaken)
         if pidx in pidx_times:
@@ -213,7 +191,17 @@ def plot_two_arm():
     n_objss = [1]
     print  "****Ranking function****"
     target_dir = 'cloud_results/1533b3c/sahs_results/uses_rrt/domain_two_arm_mover/n_objs_pack_1/qlearned_hcount_old_number_in_goal/q_config_num_train_5000_mse_weight_0.0_use_region_agnostic_True_mix_rate_1.0/n_mp_limit_5_n_iter_limit_2000/'
-    pidx_nodes_2, pidx_times_2 = get_n_nodes(target_dir)
+    pidx_nodes, pidx_times, successes, n_nodes, n_data, pidx_iks = get_n_nodes(target_dir)
+    target_dir = 'cloud_results/187aad7/sahs_results/domain_two_arm_mover/n_objs_pack_1/qlearned_hcount_old_number_in_goal/q_config_num_train_5000_mse_weight_0.0_use_region_agnostic_True/n_mp_limit_5_n_iter_limit_2000/'
+    pidx_nodes2, pidx_times2, successes2, n_nodes, n_data, pidx_iks = get_n_nodes(target_dir)
+    print np.mean(np.hstack(pidx_times.values())), np.mean(np.hstack(pidx_times2.values())), np.median(
+        np.hstack(pidx_times.values())), np.median(np.hstack(pidx_times2.values()))
+    print np.mean(np.hstack(pidx_nodes.values())), np.mean(np.hstack(pidx_nodes2.values())), np.median(
+        np.hstack(pidx_nodes.values())), np.median(np.hstack(pidx_nodes2.values()))
+    print np.mean(successes), np.mean(successes)
+    import pdb;
+    pdb.set_trace()
+
     print  "****WGANGP 1000 data points****"
     target_dir = 'cloud_results/8db0c37/sahs_results/domain_two_arm_mover/n_objs_pack_1/qlearned_hcount_old_number_in_goal/q_config_num_train_5000_mse_weight_0.0_use_region_agnostic_True/using_learned_sampler/1000/sampler_seed_0/wgangp/n_mp_limit_5_n_iter_limit_2000/'
     pidx_nodes_2, pidx_times_2 = get_n_nodes(target_dir)
@@ -355,26 +343,44 @@ def plot_two_arm():
         plt.savefig("../IJRR_GTAMP/figures/{}_arm_{}_obj.eps".format('two_arm_mover', n_objs))
 
 
+
 def print_epoch_test_results():
-    target_dir = 'test_results/sahs_results/domain_two_arm_mover/n_objs_pack_1/qlearned_hcount_old_number_in_goal/q_config_num_train_5000_mse_weight_0.0_use_region_agnostic_True/n_mp_limit_5_n_iter_limit_2000/'
-    pidx_nodes, pidx_times, successes, n_nodes, n_data, pidx_iks = get_n_nodes(target_dir)
+    # place loading
+    seed = '-1_-1_3'  # best epoch: 11386 - median 17, mean 19, success rate 1; evaluated 340 epochs
+    seed = '-1_-1_4'  # best epoch: 7492 - median 18.5, mean 28.55, success rate 0.94; evaluated 437 epochs
+    seed = '-1_-1_5'  # best epoch 10901? evaluated 313 epochs
+    seed = '-1_-1_12' # best epoch: 9232 - median 13, mean 19, success rate 1; evaluated 487 epochs; I may have to evaluate more
+
+    # pick
+    seed = '2_-1_-1'  # best epoch 26613
+    seed = '11_-1_-1' # best_epoch 9840 evaluated 290 epochs; smpler seed idx 1
+    seed = '15_-1_-1' # best epoch 7935
+    seed = '20_-1_-1' # best epoch 8854
+
+    # place home
+    # uniform 0.87, 39, 47.23
+    seed = '-1_4_-1'  # 8421 0.93 34 40.1; 6842 0.89 32.5 41.85; 8565 0.87 35.5 45.43
+    seed = '-1_6_-1'  # 8395 0.93 34 40.19; 8389 0.93 34 42.09
+    seed = '-1_10_-1' # 6013 0.9 35.5 44.51
+    seed = '-1_2_-1'  # 11138 0.83 39 48.83; 10945 0.9375 31.5 38.5
+
+    n_objs = 4
+    is_valid_idxs = False
+    target_dir = 'test_results/sahs_results/domain_two_arm_mover/n_objs_pack_{}/' \
+                 'qlearned_hcount_old_number_in_goal/q_config_num_train_5000_mse_weight_0.0_use_region_agnostic_True/' \
+                 'n_mp_limit_5_n_iter_limit_2000/'.format(n_objs)
+    pidx_nodes, pidx_times, successes, n_nodes, n_data, pidx_iks = get_n_nodes(target_dir,is_valid_idxs)
     print 'n_data {} successes {} n nodes median {} mean {} std {} n_iks {}'.format(n_data, np.mean(successes),
                                                                                     np.median(n_nodes),
                                                                                     np.mean(n_nodes),
                                                                                     np.std(n_nodes) * 1.96 / np.sqrt(
-                                                                                        n_data), np.mean(np.hstack(pidx_iks.values())))
-    seed = '2_-1_-1'
-    seed = '2_-1_3'
-    seed = '-1_-1_3'  # best epoch: 11386 - median 17, mean 19, success rate 1; evaluated 340 epochs
-    seed = '-1_-1_4'  # best epoch: 7492 - median 18.5, mean 28.55, success rate 0.94; evaluated 437 epochs
-    seed = '-1_-1_12'  # best epoch: 9232 - median 13, mean 19, success rate 1; evaluated 487 epochs; I may have to evaluate more
-    seed = '11_-1_-1'  # best_epoch 9840 evaluated 290 epochs; smpler seed idx 1
-    seed = '-1_-1_5'  # best epoch: 9206; 10901? evaluated 313 epochs
+                                                                                        n_data), np.mean(
+            np.hstack(pidx_iks.values())))
 
     # place loading seeds: 3,4,12 we are missing sampler seed idx 2
-    seed_dir = 'test_results/sahs_results/domain_two_arm_mover/n_objs_pack_1/' \
+    seed_dir = 'test_results/sahs_results/domain_two_arm_mover/n_objs_pack_{}/' \
                'qlearned_hcount_old_number_in_goal/q_config_num_train_5000_mse_weight_0.0_use_region_agnostic_True/' \
-               'using_learned_sampler/1000/sampler_seed_{}/'.format(seed)
+               'using_learned_sampler/1000/sampler_seed_{}/'.format(n_objs, seed)
     min_n_nodes = np.inf
     epoch_dir_list = os.listdir(seed_dir)
     if '-1_-1_3' in seed:
@@ -386,16 +392,35 @@ def print_epoch_test_results():
     elif '-1_-1_5' in seed:
         epoch_dir_list = ['sampler_epoch_-1_-1_9206']
         epoch_dir_list = ['sampler_epoch_-1_-1_10901']
+    elif '2_-1_-1' in seed:
+        epoch_dir_list = ['sampler_epoch_26613_-1_-1']
+    elif '11_-1_-1' in seed:
+        pass
+    elif '15_-1_-1' in seed:
+        pass
+    elif '20_-1_-1' in seed:
+        pass
+    elif '-1_10_-1' in seed:
+        epoch_dir_list = ['sampler_epoch_-1_6013_-1']
+    elif '-1_4_-1' in seed:
+        epoch_dir_list = ['sampler_epoch_-1_6842_-1']
+        epoch_dir_list = ['sampler_epoch_-1_8421_-1', 'sampler_epoch_-1_6842_-1', 'sampler_epoch_-1_8565_-1']
+    elif '-1_2_-1' in seed:
+        epoch_dir_list = ['sampler_epoch_-1_11138_-1', 'sampler_epoch_-1_10945_-1']
+    elif '-1_6_-1' in seed:
+        epoch_dir_list = ['sampler_epoch_-1_8395_-1', 'sampler_epoch_-1_8389_-1']
+
     print "Total evaluated epochs:", len(epoch_dir_list)
     for epoch_dir in epoch_dir_list:
         target_dir = seed_dir + '/' + epoch_dir + '/wgandi/n_mp_limit_5_n_iter_limit_2000/'
-        pidx_nodes, pidx_times, successes, n_nodes, n_data, pidx_iks = get_n_nodes(target_dir)
+        pidx_nodes, pidx_times, successes, n_nodes, n_data, pidx_iks = get_n_nodes(target_dir, is_valid_idxs)
         n_probs = len(pidx_nodes.keys())
+        # print n_probs
         if n_probs >= 9:  # and n_data>=36:
-            if len(epoch_dir_list) == 1:
-                condition = np.median(n_nodes) < np.median(min_n_nodes)
+            if len(epoch_dir_list) < 10:
+                condition = True#np.median(n_nodes) < np.median(min_n_nodes)
             else:
-                condition = np.median(n_nodes) < np.median(min_n_nodes) #and np.mean(successes) == 1
+                condition = np.median(n_nodes) < np.median(min_n_nodes)  and np.mean(successes) == 1
 
             if condition:
                 best_epoch = epoch_dir
@@ -411,6 +436,7 @@ def print_epoch_test_results():
                                                                                                     n_data), np.mean(
                         np.hstack(pidx_iks.values())))
                 print "======="
+    import pdb;pdb.set_trace()
     # print epoch_dir, np.median(min_n_nodes), np.mean(min_n_nodes), np.mean(min_successes), np.mean(np.hstack(pidx_iks.values()))
 
 
