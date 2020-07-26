ba62ed1303666b33587f2a9c80b3ced492b9891f
beomjoon <beomjoon@mit.edu>
2020-07-26 11:31:15 -0400

    moving get n nodes in print smpler

--- plotters/print_sampler_epoch_tests.py
+++ plotters/print_sampler_epoch_tests.py
@@ -3,6 +3,127 @@ from compare_n_nodes import get_n_nodes
 import os
 
 
+def get_n_nodes(target_dir, is_valid_idxs):
+    test_files = os.listdir(target_dir)
+    n_iks = []
+    n_nodes = []
+    n_mps = []
+
+    if 'rsc' in target_dir:
+        test_file_pidxs = [int(filename.split('pidx_')[1].split('.pkl')[0]) for filename in test_files if
+                           'pkl' in filename]
+    else:
+        test_file_pidxs = [int(filename.split('pidx_')[1].split('_')[0]) for filename in test_files if
+                           'pkl' in filename]
+    test_files = np.array(test_files)[np.argsort(test_file_pidxs)]
+    if 'two_arm' in target_dir:
+        if is_valid_idxs:
+            target_pidxs = [40200, 40201, 40202, 40204, 40205, 40206, 40207, 40208, 40209]
+        else:
+            if 'n_objs_pack_1' in target_dir:
+                target_pidxs = [40064, 40071, 40077, 40078, 40080, 40083, 40088, 40097, 40098, 40003, 40007, 40012,
+                                40018,
+                                40020, 40023, 40030, 40032, 40033, 40036, 40038, 40047, 40055, 40059, 40060, 40062]
+            else:
+                target_pidxs = [40000, 40002, 40003, 40004, 40005, 40007, 40008, 40010, 40012, 40014, 40015, 40016,
+                                40017,
+                                40019, 40021, 40023, 40024, 40025, 40026, 40028, 40030, 40031, 40033, 40035, 40036]
+    else:
+        target_pidxs = [20001, 20002, 20003, 20004, 20008, 20009, 20011, 20015, 20019, 20021, 20023, 20024, 20035,
+                        20046, 20047, 20051, 20053, 20056, 20057, 20061, 20063, 20066, 20067, 20069, 20072, 20075,
+                        20080, 20083, 20084, 20086, 20093, 20094, 20095]
+        target_pidxs = [20001, 20002, 20003, 20004, 20008, 20009, 20011, 20019, 20021, 20024, 20035, 20047, 20051,
+                        20053, 20057, 20061, 20063, 20066, 20069, 20072, 20075, 20084, 20086, 20093, 20094, 20095]
+        target_pidxs = range(20000, 20100)
+
+        # target_pidxs = [20002, 20004, 20008, 20009, 20011, 20019, 20021, 20024, 20035, 20051, 20061, 20063, 20066, 20069, 20072, 20075,
+        # 20093]
+
+        # target_pidxs.remove(20055)
+        # target_pidxs.remove(20090)
+        # target_pidxs.remove(20093)
+        # target_pidxs = [20000, 20001, 20005, 20009, 20011, 20023, 20027, 20030, 20035, 20046, 20060, 20061, 20076]
+
+    targets = []
+    for pidx in target_pidxs:
+        for i in range(5):
+            targets.append((pidx, i))
+
+    # print "number of target pidxs", len(target_pidxs)
+    successes = []
+    times = []
+    pidx_times = {}
+    pidx_nodes = {}
+    pidx_iks = {}
+    for filename in test_files:
+        if 'pkl' not in filename:
+            print 'File skipped', filename
+            continue
+        if 'gnn_seed' in filename:
+            absqseed = int(filename.split('gnn_seed_')[-1].split('.pkl')[0])
+
+            # target_absqseed = 1 if 'a179000' in target_dir else 0
+            target_absqseed = 0
+            # if absqseed != target_absqseed:
+            # print filename
+            #    continue
+
+        if 'rsc' in target_dir:
+            pidx = int(filename.split('pidx_')[1].split('.pkl')[0])
+        else:
+            pidx = int(filename.split('pidx_')[1].split('_')[0])
+
+        seed = int(filename.split('seed_')[1].split('_')[0])
+        if not pidx in target_pidxs:
+            continue
+        fin = pickle.load(open(target_dir + filename, 'r'))
+        # targets.remove((pidx, seed))
+
+        if 'num_nodes' in fin:
+            n_node = fin['num_nodes']
+        else:
+            n_node = fin['n_nodes']
+
+        n_nodes.append(n_node)
+
+        if 'two_arm' in target_dir:
+            if 'n_objs_pack_4' in target_dir:
+                timelimit = 2000
+            else:
+                timelimit = 2000
+        else:
+            timelimit = 1000
+
+        """
+        if fin['tottime'] >= timelimit:
+            successes.append(False)
+            timetaken = timelimit
+        else:
+            successes.append(fin['success'])
+            timetaken = fin['tottime']
+        """
+        successes.append(fin['success'])
+        timetaken = fin['tottime']
+
+
+        times.append(timetaken)
+        if pidx in pidx_times:
+            pidx_nodes[pidx].append(n_node)
+            pidx_times[pidx].append(timetaken)
+            pidx_iks[pidx].append(fin['n_feasibility_checks']['ik'])
+        else:
+            pidx_nodes[pidx] = [n_node]
+            pidx_times[pidx] = [timetaken]
+            pidx_iks[pidx] = [fin['n_feasibility_checks']['ik']]
+
+    n_data = len(n_nodes)
+    # print "*****REMAINING****", len(targets)
+    # for pidx in targets:
+    #    pidx_times[pidx_times.keys()[0]].append(timelimit)
+    #    successes.append(False)
+
+    return pidx_nodes, pidx_times, successes, n_nodes, n_data, pidx_iks
+
 def get_sampler_dir(algo_name, learned_sampler_atype):
     # place loading
     if algo_name == 'wgandi':
@@ -65,15 +186,16 @@ def get_target_epoch_dir(seed_dir, is_valid_idxs):
             condition = n_data >= 9
         else:
             condition = n_data > 36
-        print epoch_dir, n_data
+        #print epoch_dir, n_data
         if condition:
             target_dirs.append(epoch_dir)
+    print len(target_dirs)
     return target_dirs
 
 
 def print_epoch_test_results():
     # print all the epochs that have more than 36 problems?
-    algo_name = 'wgangp'
+    algo_name = 'wgandi'
     learned_sampler_atype = 'pick'
     seed_dirs = get_sampler_dir(algo_name, learned_sampler_atype)
     is_valid_idxs = False
