43679b3d40fb7e36d23379dbbb4cde25304a32fd
beomjoon <beomjoon@mit.edu>
2020-07-30 09:23:36 -0400

    move the done command

--- upload_greedy_results.py
+++ upload_greedy_results.py
@@ -8,8 +8,8 @@ file_name = 'test_results_{}.zip'.format(socket.gethostname())
 os.system('rm {}'.format(file_name))
 print 'Zipping....'
 cmd = 'zip -r -qq {} ./test_results'.format(file_name)
-print 'Done!'
 os.system(cmd)
+print 'Done!'
 
 cmd = 'mc rm csail/bkim/guiding-gtamp/{}'.format(file_name)
 print cmd
